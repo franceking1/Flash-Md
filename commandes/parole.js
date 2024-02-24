856cf96f3c2e474bf7637cca4b1041ace840f95a
France King <151246926+franceking1@users.noreply.github.com>
2024-02-24 16:08:16 +0300

    lyrics.js

---
+++ commandes/parole.js
@@ -0,0 +1,36 @@
+const {zokou} =require("../framework/zokou");
+const axios =require("axios");
+
+
+zokou({ nomCom: "lyrics",
+        reaction: "✨",
+        categorie: "Search" }, async (dest, zk, commandeOptions) => {
+    
+    const { repondre, arg, ms } = commandeOptions;  
+        
+   try {
+
+    if (!arg || arg.length === 0) return repondre("please provide me the song name");
+
+    let  result  = await axios.get(`https://vihangayt.me/search/lyrics?q=${arg.join(' ')}`);
+
+    let lyrics = result.data.data;
+
+    if (lyrics.error) return repondre("no lyrics found");
+
+    let msg = `---------FLASH-MD-lyrics-finder--------
+
+* *Artist :* ${lyrics.artist}
+
+
+* *Title :* ${lyrics.title}
+
+
+${lyrics.lyrics}`
+
+    zk.sendMessage(dest,{image : { url : './media/lyrics-img.jpg'} , caption : msg}, { quoted : ms });
+    
+   } catch (err) {
+       repondre('Error')
+   }
+        })
