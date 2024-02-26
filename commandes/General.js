8d6229ef82851d6f4b4a367b8a69a191edc7f7f6
France King <151246926+franceking1@users.noreply.github.com>
2024-02-26 10:33:52 +0300

    General.js

--- commandes/General.js
+++ commandes/General.js
@@ -60,7 +60,7 @@ zokou({ nomCom: "dev", categorie: "General", reaction: "💞" }, async (dest, zk
       // Ajoute d'autres développeurs ici avec leur nom et numéro
     ];
 
-    let message = "👋 welcome to Flash-Md ! here is the dev :\n\n";
+    let message = "👋 *Welcome to Flash-Md ! here is the developer numbers:\n\n";
     for (const dev of devs) {
       message += `----------------\n• ${dev.nom} : https://wa.me/${dev.numero}\n`;
     }
