ea78ec1968113c2911b375cddf9b9404b863f9a9
France King <151246926+franceking1@users.noreply.github.com>
2024-03-05 02:11:06 +0300

    TEST ping.js

--- commandes/ping.js
+++ commandes/ping.js
@@ -15,7 +15,7 @@ zokou({ nomCom: 'ping',
   async (dest, zk, commandeOptions) => {
     const { ms, arg, repondre } = commandeOptions;
     const { start} = new Date().getTime()
-    return repondre('*Pong!!*'\n ```'977'``` + '*ms*')
+    return repondre('*Pong!*\n ```' + 987 + '``` *ms*')
     const { end } = new Date().getTime()
     await zok.sendMessage('*Pong!*\n ```' + (end - start) + '``` *ms*')
   }
