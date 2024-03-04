db47f902e5f2c53ef10538670bb725b2070d984c
France King <151246926+franceking1@users.noreply.github.com>
2024-03-05 00:39:48 +0300

    TEST ping.js

--- commandes/ping.js
+++ commandes/ping.js
@@ -1,6 +1,7 @@
 const { zokou } = require("../framework/zokou");
 const moment = require("moment-timezone");
-
+const { default: axios } = require('axios');
+//const conf = require('../set');
 
 
 zokou({ nomCom: 'ping',
@@ -13,10 +14,10 @@ zokou({ nomCom: 'ping',
   },
   async (dest, zk, commandeOptions) => {
     const { ms, arg, repondre } = commandeOptions;
-    const { start = new Date().getTime()} 
+    const { start} = new Date().getTime()
     return repondre('```Pinging!!!!```')
-    const { end = new Date().getTime()} 
-    await repondre('*Pong!*\n ```' + (end - start) + '``` *ms*')
+    const { end } = new Date().getTime()
+    const response = await axios.get('*Pong!*\n ```' + (end - start) + '``` *ms*')
   }
 )
 
