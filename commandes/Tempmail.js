b13d06ba4f4e1fad2da994904298763447bdc66c
France King <151246926+franceking1@users.noreply.github.com>
2024-03-05 17:26:37 +0300

    Tempmail.js

--- commandes/Tempmail.js
+++ commandes/Tempmail.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
 zokou({ nomCom: "tempmail", reaction: "😌", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = 'Temporary emails powered by *FLASH-MD*,created by *France* \n\n ' + "Click the link below to create unlimited TEMPORARY Emails. Powered by FLASH-MD.*";
+    let z = '*FLASH-MD* Temporary emails,Powered by *France King* \n\n ' + "Click the link below to create unlimited TEMPORARY Emails. Powered by *FLASH-MD.*";
     let d = ' https://extraclass.ng';
     let varmess = z + d;
     var img = 'https://telegra.ph/file/f1f2303ff4e39b0a3b6b3.jpg';
