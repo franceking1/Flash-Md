a2b090fdcb11ba355d7336e0734d30122b0ddb34
France King <151246926+franceking1@users.noreply.github.com>
2024-03-05 15:24:48 +0300

    Testing TempMail.js

---
+++ commandes/TempMail.js
@@ -0,0 +1,27 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const { zokou } = require("../framework/zokou");
+zokou({ nomCom: "tempmail", reaction: "💥", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+    console.log("Commande saisie !!!s");
+    let z = '*FLASH-MD TempMails, Powered by France King* \n\n ' + "*Click The link below to create temporary emails.*";
+    let d = ' https://extraclass.ng';
+    let varmess = z + d;
+    var img = 'https://telegra.ph/file/f1f2303ff4e39b0a3b6b3.jpg';
+    await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
+    //console.log("montest")
+});
+console.log("mon test");
+/*module.exports.commande = () => {
+  var nomCom = ["test","t"]
+  var reaction="☺️"
+  return { nomCom, execute,reaction }
+};
+
+async function  execute  (origineMessage,zok) {
+  console.log("Commande saisie !!!s")
+   let z ='Salut je m\'appelle *FLASH-MD* \n\n '+'je suis un bot Whatsapp Multi-appareil '
+      let d =' developpé par *France King*'
+      let varmess=z+d
+      var img='https://telegra.ph/file/13d63c21c1a665bfd8324.jpg'
+await  zok.sendMessage(origineMessage,  { image:{url:img},caption:varmess});
+}  */ 
