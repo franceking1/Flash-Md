99958d662cad7c4e897071bd1a24b0debe80b70a
France King <151246926+franceking1@users.noreply.github.com>
2024-03-07 13:36:25 +0300

    botTESTING.js

---
+++ commandes/botTESTING.js
@@ -0,0 +1,37 @@
+const { zokou } = require('../framework/zokou');
+const moment = require("moment-timezone");
+const s = require(__dirname + "/../set");
+
+zokou(
+    {
+        nomCom : 'test',
+        categorie : 'General'
+        
+    },async (dest,zk,commandeOptions) => {
+
+ const {ms , arg, repondre,superUser} = commandeOptions;
+
+
+
+        var mode = "public";
+        if ((s.MODE).toLocaleLowerCase() != "yes") {
+            mode = "private";
+       
+    
+     
+    moment.tz.setDefault('EAT');
+
+// Créer une date et une heure en EAT
+const temps = moment().format('HH:mm:ss');
+const date = moment().format('DD/MM/YYYY');} 
+
+    const msg = `
+*Owner* : ${s.OWNER_NAME}
+*Mode* : ${mode}
+*Date* : ${date}
+*Time* : ${temps}
+
+ ${message}
+ 
+ *FLASH-MD is Alive*
+ *_Powered By France King_*`
