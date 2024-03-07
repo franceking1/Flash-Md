0f9979e8e3f58c83ab45144b5365473343e58d02
France King <151246926+franceking1@users.noreply.github.com>
2024-03-07 14:15:48 +0300

    botTest.js

---
+++ commandes/botTest.js
@@ -0,0 +1,23 @@
+const { zokou } = require("../framework/zokou");
+const moment = require("moment-timezone");
+const { default: axios } = require('axios');
+//const conf = require('../set');
+
+
+zokou({ nomCom: 'test',
+    desc: 'To test the bot',
+    Categorie: 'General',
+    reaction: '🐐', 
+    fromMe: 'true', 
+
+       
+  },
+  async (dest, zk, commandeOptions) => {
+    const { ms, arg, repondre } = commandeOptions;
+    const { start} = new Date().getTime()
+    return repondre('*FLASH-MD is Alive!!!!!!!*\n ```' + powered by + '``` *France King*')
+    const { end } = new Date().getTime()
+    await zok.sendMessage('*Pong!*\n ```' + (end - start) + '``` *ms*')
+  }
+)
+ 
