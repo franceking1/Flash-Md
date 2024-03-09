a5d659bb3179aa4162701c5649e76f59518efe3d
France King <151246926+franceking1@users.noreply.github.com>
2024-03-09 15:36:38 +0300

    menu.js

--- commandes/menu.js
+++ commandes/menu.js
@@ -51,7 +51,7 @@ const date = moment().format('DD/MM/YYYY');
 `;
 
     for (const cat in coms) {
-        menuMsg += `*╭────♈* *${cat}* *♈⊷*`;
+        menuMsg += `*╭────☢️* *${cat}* *☢️⊷*`;
         for (const cmd of coms[cat]) {
             menuMsg += `
  *|☬* ${cmd}`;
