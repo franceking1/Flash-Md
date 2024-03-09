6e81ce8455f680743b1cc85ceafdfbda1de25b16
France King <151246926+franceking1@users.noreply.github.com>
2024-03-09 18:08:43 +0300

    mesfonctions.js

--- framework/mesfonctions.js
+++ framework/mesfonctions.js
@@ -9,7 +9,7 @@ const baileys_1 = require("@whiskeysockets/baileys");
 const fs = require('fs-extra');
 const util = require('util');
 let { listall } = require('./stylish-font');
-/*_________by Djalega++
+/*_________by France King
 
 fonction zJson:
 récupère un objet json
@@ -32,7 +32,7 @@ module.exports.genererNomFichier = async (extension) => {
 /** ************ */
 module.exports.stick = async (buffer, author) => {
     var sticker = new Sticker(buffer, {
-        pack: 'Zokou-MD',
+        pack: 'FLASH-MD',
         author: author,
         type: StickerTypes.FULL,
         categories: ['🤩', '🎉'],
