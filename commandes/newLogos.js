a9684709c396445c0c19af435562941ba656851c
France King <151246926+franceking1@users.noreply.github.com>
2024-03-12 13:31:23 +0300

    TESTnewLogos.js

--- commandes/newLogos.js
+++ commandes/newLogos.js
@@ -1,14 +1,26 @@
 const { zokou } = require("../framework/zokou");
 var mumaker = require("mumaker");
-zokou({nomCom : "avenger",categorie : "Logo",reaction : "😎"} , async (dest,zk,commandeOptions) =>{
-
-  const {arg,repondre,ms,prefixe} = commandeOptions;
-  if(!arg[0]) { repondre( `Example:\n ${prefixe}avenger FLASH-MD` ); return ;}
-   let text = arg.join(" ")
-   mumaker.textpro("https://en.ephoto360.com/logo-3d-style-avengers-online-427.html", text)
-.then((data) =>{
- zk.sendMessage(dest,{image : { url : data.image},caption : 'Logo by *France King*'},{quoted:ms})
-}) 
-.catch(console.log)
-})
-;
+zokou({ nomCom: "avenger", categorie: "Logo", reaction: "😎" }, async (dest, zk, commandeOptions) => {
+    let { arg, repondre, prefixe, ms } = commandeOptions;
+    try {
+        const noArgMsg = `*_Example*:  ${prefixe}avenger FranceKing`;
+        //  if(arg=='') {await zok.sendMessage(dest,{text:noArgMsg},{quoted:infoMessage}); return;}
+        if (arg == '' || !arg) {
+            repondre(noArgMsg);
+            return;
+        }
+        var lienMaker = "https://en.ephoto360.com/logo-3d-style-avengers-online-427.html";
+        var lienMaker2 = "https://en.ephoto360.com/create-logo-3d-style-avengers-style-text-effects-online-427.html";
+      
+       
+        const imgInfo = await mumaker.ephoto(lienMaker2, arg.join(' '));
+       
+        await zk.sendMessage(dest, { text: " *\t Avenging...*" }, { quoted: ms });
+       // var idImg = Object.values(imgInfo)[3];
+       
+        await zk.sendMessage(dest, { image: { url: imgInfo.image }, caption: " \t *Logo by FLASH-MD*" }, { quoted: ms });
+    }
+    catch (e) {
+        repondre("🥵🥵 " + e);
+    }
+});
