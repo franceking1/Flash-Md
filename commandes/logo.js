b51a5686256f6cf2c1df81fa299ea1f67e11c9a2
France King <151246926+franceking1@users.noreply.github.com>
2024-03-12 14:17:25 +0300

    logo.js

--- commandes/logo.js
+++ commandes/logo.js
@@ -497,3 +497,28 @@ zokou({nomCom:"incandescent",categorie:"Logo",reaction:"😋"},async(dest,zk,com
     await zk.sendMessage(dest,{image:{url:img.image},caption:" *Logo by FLASH-MD*"},{quoted:ms})
   }catch(e){repondre(e)}
 })
+
+zokou({ nomCom: "avenger", categorie: "Logo", reaction: "😎" }, async (dest, zk, commandeOptions) => {
+    let { arg, repondre, prefixe, ms } = commandeOptions;
+    try {
+        const noArgMsg = `*_Example*:  ${prefixe}avenger FranceKing`;
+        //  if(arg=='') {await zok.sendMessage(dest,{text:noArgMsg},{quoted:infoMessage}); return;}
+        if (arg == '' || !arg) {
+            repondre(noArgMsg);
+            return;
+        }
+        var lienMaker = "https://en.ephoto360.com/logo-3d-style-avengers-online-427.html";
+        var lienMaker2 = "https://en.ephoto360.com/create-logo-3d-style-avengers-text-effects-online-427.html";
+      
+       
+        const imgInfo = await mumaker.ephoto(lienMaker2, arg.join(' '));
+       
+        await zk.sendMessage(dest, { text: " *\t Avenging...*" }, { quoted: ms });
+       // var idImg = Object.values(imgInfo)[3];
+       
+        await zk.sendMessage(dest, { image: { url: imgInfo.image }, caption: " \t *Logo by FLASH-MD*" }, { quoted: ms });
+    }
+    catch (e) {
+        repondre("🥵🥵 " + e);
+    }
+})
