5e81cb26a22568b9dc7b5bc2b67d7ba43c13d544
France King <151246926+franceking1@users.noreply.github.com>
2024-03-15 11:17:21 +0300

    🐐

--- commandes/ping.js
+++ commandes/ping.js
@@ -15,7 +15,7 @@ zokou({ nomCom: 'ping',
   async (dest, zk, commandeOptions) => {
     const { ms, arg, repondre } = commandeOptions;
     const { start} = new Date().getTime()
-    return repondre('Testing successful, Bot is active\n\nping ${latensie.toFixed(4)} milliseconds')
+    return repondre('*Pong!!* \n ```' + 2000 +```' *ms*)
     const { end } = new Date().getTime()
     await zok.sendMessage('*Pong!*\n ```' + (end - start) + '``` *ms*')
   }
