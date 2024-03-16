c833bcfba8586edf45076db181f42297e458557e
France King <151246926+franceking1@users.noreply.github.com>
2024-03-16 17:31:36 +0300

    Test uptime.js

---
+++ commandes/uptime.js
@@ -0,0 +1,29 @@
+const { zokou } 
+module.exports = {
+  zokou: {
+    nomCom: "uptime",
+    aliases: ["up", "upt"],
+    version: "1.0",
+    author: "XyryllPanget",
+    role: 0,
+    shortDescription: {
+      en: "Displays the uptime of the bot."
+    },
+    longDescription: {
+      en: "Displays the amount of time that the bot has been running for."
+    },
+    categorie: "New",
+    guide: {
+      en: "Use {p}uptime to display the uptime of the bot."
+    }
+  },
+  onStart: async function ({ api, event, args }) {
+    const uptime = process.uptime();
+    const seconds = Math.floor(uptime % 60);
+    const minutes = Math.floor((uptime / 60) % 60);
+    const hours = Math.floor((uptime / (60 * 60)) % 24);
+    const days = Math.floor(uptime / (60 * 60 * 24));
+    const uptimeString = `${hours} hours ${minutes} minutes ${seconds} second`;
+    api.sendMessage(`hello user, the bot has been running for ${uptimeString}.`, event.threadID);
+  }
+};
