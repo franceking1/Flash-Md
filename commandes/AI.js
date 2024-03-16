46c84a2ddf35182aa9f72244da3dc40f77b82f49
France King <151246926+franceking1@users.noreply.github.com>
2024-03-17 02:19:23 +0300

    🐐 🔵

--- commandes/AI.js
+++ commandes/AI.js
@@ -83,7 +83,7 @@ fetch(`http://api.brainshop.ai/get?bid=177607&key=NwzhALqeO1kubFVD&uid=[uid]&msg
   
       // Regrouper les arguments en une seule chaîne séparée par "-"
       const question = arg.join(' ');
-      const response = await axios.get(`https://vihangayt.me/tools/chatgpt4?q=${question}`);
+      const response = await axios.get(`https://chat.openai.com/c/04b9daa3-0cd4-47c8-a9db-502a55225a25?q=${question}`);
       
       const data = response.data;
       if (data) {
