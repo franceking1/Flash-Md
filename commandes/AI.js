541415f9585f47df98a713e9096332994797aefd
giftedtech <admin@giftedtechnexus.co.ke>
2024-03-18 00:46:22 +0300

    Add files via upload

---
+++ commandes/AI.js
@@ -0,0 +1,101 @@
+const { zokou } = require('../framework/zokou');
+const traduire = require("../framework/traduction") ;
+const { default: axios } = require('axios');
+//const conf = require('../set');
+
+
+
+
+zokou({nomCom:"bot",reaction:"📡",categorie:"IA"},async(dest,zk,commandeOptions)=>{
+
+  const {repondre,ms,arg}=commandeOptions;
+  
+    if(!arg || !arg[0])
+    {return repondre("yes I'm listening to you.")}
+    //var quest = arg.join(' ');
+  try{
+    
+    
+const message = await traduire(arg.join(' '),{ to : 'en'});
+ console.log(message)
+fetch(`http://api.brainshop.ai/get?bid=177607&key=NwzhALqeO1kubFVD&uid=[uid]&msg=${message}`)
+.then(response => response.json())
+.then(data => {
+  const botResponse = data.cnt;
+  console.log(botResponse);
+
+  traduire(botResponse, { to: 'en' })
+    .then(translatedResponse => {
+      repondre(translatedResponse);
+    })
+    .catch(error => {
+      console.error('Error when translating into French :', error);
+      repondre('Error when translating into French');
+    });
+})
+.catch(error => {
+  console.error('Error requesting BrainShop :', error);
+  repondre('Error requesting BrainShop');
+});
+
+  }catch(e){ repondre("oops an error : "+e)}
+    
+  
+  });  
+
+
+
+  zokou({ nomCom: "dalle", reaction: "📡", categorie: "IA" }, async (dest, zk, commandeOptions) => {
+    const { repondre, arg, ms } = commandeOptions;
+  
+    try {
+      if (!arg || arg.length === 0) {
+        return repondre(`Please enter the necessary information to generate the image.`);
+      }
+  
+      // Regrouper les arguments en une seule chaîne séparée par "-"
+      const image = arg.join(' ');
+      const response = await axios.get(`https://vihangayt.me/tools/photoleap?q=${image}`);
+      
+      const data = response.data;
+      let caption = '*powered by FLASH-MD*';
+      
+      if (data.status && data.owner && data.data) {
+        // Utiliser les données retournées par le service
+        const imageUrl = data.data;
+        zk.sendMessage(dest, { image: { url: imageUrl }, caption: caption }, { quoted: ms });
+      } else {
+        repondre("Error during image generation.");
+      }
+    } catch (error) {
+      console.error('Erreur:', error.message || 'Une erreur s\'est produite');
+      repondre("Oops, an error occurred while processing your request");
+    }
+  });
+  
+  zokou({ nomCom: "gpt", reaction: "📡", categorie: "IA" }, async (dest, zk, commandeOptions) => {
+    const { repondre, arg, ms } = commandeOptions;
+  
+    try {
+      if (!arg || arg.length === 0) {
+        return repondre(`Please ask a question.`);
+      }
+  
+      // Regrouper les arguments en une seule chaîne séparée par "-"
+      const question = arg.join(' ');
+      const response = await axios.get(`https://giftedgpt.vercel.app?q=${question}`);
+      
+      const data = response.data;
+      if (data) {
+        repondre(data.data);
+      } else {
+        repondre("Error during response generation.");
+      }
+    } catch (error) {
+      console.error('Erreur:', error.message || 'Une erreur s\'est produite');
+      repondre("Oops, an error occurred while processing your request.");
+    }
+  });
+
+
+  
