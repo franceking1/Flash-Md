0b4bcb974a053dcfc4980e5634e2d75103260d83
France King <151246926+franceking1@users.noreply.github.com>
2024-03-18 09:55:03 +0300

    🐐 🔵

---
+++ commandes/quotes.js
@@ -0,0 +1,12 @@
+const { zokou } = require('../framework/zokou');
+const traduire = require("../framework/traduction") ;
+const { default: axios } = require('axios'); 
+
+
+  zokou({ nomCom: "quote", reaction: "🌟", categorie: "NEW" }, async (dest, zk, commandeOptions) => {
+    const { repondre, arg, ms } = commandeOptions;
+  
+  
+      const response = await axios.get(`https://some-random-api.com/animu/quote}`);
+      
+      
