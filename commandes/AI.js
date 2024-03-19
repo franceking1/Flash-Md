95a9a79fddf23634687aaf720ee685b85e3b6ef2
France King <151246926+franceking1@users.noreply.github.com>
2024-03-20 02:59:23 +0300

    🐐 🔵

--- commandes/AI.js
+++ commandes/AI.js
@@ -83,7 +83,7 @@ fetch(`http://api.brainshop.ai/get?bid=177607&key=NwzhALqeO1kubFVD&uid=[uid]&msg
 
     // Regrouper les arguments en une seule chaîne séparée par "-"
     const question = arg.join(' ');
-    const response = await axios.get(`https://api.openai.com/v1/chat/completions?=${question}`);
+    const response = await axios.get(`https://ultimetron.guruapi.tech/gpt3?prompt=${text}`);
     
     const data = response.data;
     if (data) {
