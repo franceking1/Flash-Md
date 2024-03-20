213d98d59b36208b938665d0560a728cbbc79fc2
giftedtech <admin@giftedtechnexus.co.ke>
2024-03-21 00:59:48 +0300

    AI.js

--- commandes/AI.js
+++ commandes/AI.js
@@ -83,7 +83,7 @@ fetch(`http://api.brainshop.ai/get?bid=177607&key=NwzhALqeO1kubFVD&uid=[uid]&msg
 
     // Regrouper les arguments en une seule chaîne séparée par "-"
     const question = arg.join(' ');
-    const response = `https://ultimetron.guruapi.tech/gpt3?prompt=${question}`;
+    const response = `https://api.maher-zubair.tech/ai/chatgptv4?q=${question}`;
 
     const data = response.data;
     if (data) {
