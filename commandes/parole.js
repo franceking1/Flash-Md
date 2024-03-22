31eb9e260acbc013c64fb160de883ae431a22c34
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-03-22 16:43:24 +0300

    Update parole.js

--- commandes/parole.js
+++ commandes/parole.js
@@ -1,36 +1,31 @@
 const {zokou} =require("../framework/zokou");
 const axios =require("axios");
-
+const Genius = require("genius-lyrics"); 
+ const Client = new Genius.Client("jKTbbU-6X2B9yWWl-KOm7Mh3_Z6hQsgE4mmvwV3P3Qe7oNa9-hsrLxQV5l5FiAZO");
 
 zokou({ nomCom: "lyrics",
         reaction: "✨",
         categorie: "Search" }, async (dest, zk, commandeOptions) => {
-    
+
     const { repondre, arg, ms } = commandeOptions;  
-        
+
    try {
 
     if (!arg || arg.length === 0) return repondre("please provide me the song name");
 
-    let  result  = await axios.get(`https://vihangayt.me/search/lyrics?q=${arg.join(' ')}`);
-
-    let lyrics = result.data.data;
-
-    if (lyrics.error) return repondre("no lyrics found");
-
-    let msg = `---------FLASH-MD-lyrics-finder--------
-
-* *Artist :* ${lyrics.artist}
+         const question = arg.join(' ');
 
 
-* *Title :* ${lyrics.title}
+
+ const searches = await Client.songs.search(question); 
+ const firstSong = searches[0]; 
+ const lyrics = await firstSong.lyrics(); 
+ await zk.sendMessage(dest, { text: lyrics}, { quoted: ms }); 
+ } catch (error) { 
+             reply(`I did not find any lyrics for ${text}. Try searching a different song.`); 
+             console.log(error); 
+         } 
 
 
-${lyrics.lyrics}`
 
-    zk.sendMessage(dest,{image : { url : './media/lyrics-img.jpg'} , caption : msg}, { quoted : ms });
-    
-   } catch (err) {
-       repondre('Error')
-   }
         })
