3d41647e8fdd37e8fce1cb712cd12e95fc939b83
France King <151246926+franceking1@users.noreply.github.com>
2024-03-22 17:31:57 +0300

    🐐 🔵

--- commandes/parole.js
+++ commandes/parole.js
@@ -1,28 +1,28 @@
 const {zokou} =require("../framework/zokou");
 const axios =require("axios");
 const Genius = require("genius-lyrics"); 
-const Client = new Genius.Client("jKTbbU-6X2B9yWWl-KOm7Mh3_Z6hQsgE4mmvwV3P3Qe7oNa9-hsrLxQV5l5FiAZO");
+ const Client = new Genius.Client("jKTbbU-6X2B9yWWl-KOm7Mh3_Z6hQsgE4mmvwV3P3Qe7oNa9-hsrLxQV5l5FiAZO");
 
 zokou({ nomCom: "lyrics",
         reaction: "✨",
         categorie: "Search" }, async (dest, zk, commandeOptions) => {
-
+    
     const { repondre, arg, ms } = commandeOptions;  
-
+        
    try {
 
     if (!arg || arg.length === 0) return repondre("please provide me the song name");
 
          const question = arg.join(' ');
 
-
-
+ 
+  
  const searches = await Client.songs.search(question); 
  const firstSong = searches[0]; 
  const lyrics = await firstSong.lyrics(); 
  await zk.sendMessage(dest, { text: lyrics}, { quoted: ms }); 
  } catch (error) { 
-             repondre(`I did not find any lyrics for ${text}. Try searching a different song.`); 
+             reply(`I did not find any lyrics for ${text}. Try searching a different song.`); 
              console.log(error); 
          } 
 
