9f7b350b5455b7341a530a19bad428e67933143d
France King <151246926+franceking1@users.noreply.github.com>
2024-03-23 00:37:50 +0300

    🐐 🔵

--- commandes/parole.js
+++ commandes/parole.js
@@ -1,7 +1,7 @@
-const {zokou} =require("../framework/zokou");
-const axios =require("axios");
+const { zokou } = require("../framework/zokou");
+const axios = require("axios");
 const Genius = require("genius-lyrics"); 
- const Client = new Genius.Client("jKTbbU-6X2B9yWWl-KOm7Mh3_Z6hQsgE4mmvwV3P3Qe7oNa9-hsrLxQV5l5FiAZO");
+const Client = new Genius.Client("jKTbbU-6X2B9yWWl-KOm7Mh3_Z6hQsgE4mmvwV3P3Qe7oNa9-hsrLxQV5l5FiAZO");
 
 zokou({ nomCom: "lyrics",
         reaction: "✨",
