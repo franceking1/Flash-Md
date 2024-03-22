cc51b10d60f6d0f657cf0c759a134bee1c666009
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-03-22 16:43:08 +0300

    Update AI.js

--- commandes/AI.js
+++ commandes/AI.js
@@ -73,28 +73,23 @@ fetch(`http://api.brainshop.ai/get?bid=177607&key=NwzhALqeO1kubFVD&uid=[uid]&msg
     }
   });
   
-  zokou({ nomCom: "gpt", reaction: "📡", categorie: "IA" }, async (dest, zk, commandeOptions) => {
+    zokou({ nomCom: "gpt", reaction: "📡", categorie: "IA" }, async (dest, zk, commandeOptions) => {
     const { repondre, arg, ms } = commandeOptions;
-  
-    try {
+
+
       if (!arg || arg.length === 0) {
         return repondre(`Please ask a question.`);
       }
-  
+
       // Regrouper les arguments en une seule chaîne séparée par "-"
       const question = arg.join(' ');
-      const response = await axios.get(`https://api.maher-zubair.tech/ai/chatgptv4?q=${question}`);
-      
-      const data = response.data;
-      if (data) {
-        repondre(data.data);
-      } else {
-        repondre("Error during response generation.");
-      }
-    } catch (error) {
-      console.error('Erreur:', error.message || 'Une erreur s\'est produite');
-      repondre("Oops, an error occurred while processing your request.");
-    }
+     const response = await fetch(`https://api.maher-zubair.tech/ai/chatgptv4?q=${question}`);
+const data = await response.json();
+
+await repondre(data.result);
+console.log(data.completion); 
+
+
   });
 
 
