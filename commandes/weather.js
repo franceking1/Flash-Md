e8f94b026f01dd33839e635819b2b192004c767c
France King <151246926+franceking1@users.noreply.github.com>
2024-03-23 10:24:55 +0300

    🐐 🔵

--- commandes/weather.js
+++ commandes/weather.js
@@ -1,8 +1,5 @@
 const axios = require('axios');
-const fs = require('fs');
 const {zokou} = require('../framework/zokou');
-const { writeFile } = require('fs/promises')
-
 zokou({nomCom : "weather" , categorie : "NEW"},async (dest , zk , commandeOptions)=>{
   const {ms,repondre,arg} = commandeOptions ;
 if (!text) return repondre("Give me location!!");
