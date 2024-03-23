e58d1c1a80a08e64c2d7437f73f131086cafa2d1
France King <151246926+franceking1@users.noreply.github.com>
2024-03-23 10:30:17 +0300

    Testing weather.js

--- commandes/weather.js
+++ commandes/weather.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 const {zokou} = require('../framework/zokou');
 zokou({nomCom : "weather" , categorie : "NEW"},async (dest , zk , commandeOptions)=>{
-  const {ms,repondre,arg} = commandeOptions ;
+  const {text,repondre,textw} = commandeOptions ;
 if (!text) return repondre("Give me location!!");
             const response = await axios.get(
                 `https://api.openweathermap.org/data/2.5/weather?q=${text}&units=metric&appid=060a6bcfa19809c2cd4d97a212b19273&language=en`
