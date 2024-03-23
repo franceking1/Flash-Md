406bf176ac4de70ef80b3ea28d0f3d3f89f84bcc
France King <151246926+franceking1@users.noreply.github.com>
2024-03-24 01:32:15 +0300

    Test tfactos.js

--- commandes/factos.js
+++ commandes/factos.js
@@ -4,7 +4,7 @@ const { default: axios } = require('axios');
 
 
 zokou({
-        pattern: "fact",
+        nomCom: "fact",
         desc: "Sends fact in chat.",
         categorie: "NEW",
         reaction: "🙌" ,
