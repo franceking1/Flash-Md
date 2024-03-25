1159d08e526435fe3355de9333dff225615464c6
France King <151246926+franceking1@users.noreply.github.com>
2024-03-25 16:29:03 +0300

    🐐 🔵

--- commandes/test.js
+++ commandes/test.js
@@ -1,27 +1,51 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const { france } = require("../framework/france");
-france({ nomCom: "repo", reaction: "😌", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
-    console.log("Commande saisie !!!s");
-    let z = 'Hello This is  *FLASH-MD* \n\n ' + "The Following is *FLASH-MD Repo.*";
+const { zokou } = require("../framework/zokou");
+zokou({ nomCom: "repo", reaction: "😌", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+
+
+const githubRepo = 'https://api.github.com/repos/franceking1/Flash-Md';
+const img = 'https://telegra.ph/file/f1f2303ff4e39b0a3b6b3.jpg';
+
+
+    const response = await fetch(githubRepo); 
+        const data = await response.json(); 
+
+        if (data) {
+            const repoInfo = {
+                stars: data.stargazers_count,
+                forks: data.forks_count,
+                lastUpdate: data.updated_at,
+                owner: data.owner.login
+            };
+const releaseDate = new Date(data.created_at).toLocaleDateString('en-GB');
+            const lastUpdateDate = new Date(repoInfo.lastUpdate).toLocaleDateString('en-GB');
+
+const gitdata = `Hello 👋 
+This is *FLASH-MD* The following is *FLASH-MD's*
+*REPOSITORY:* ${data.html_url}
+✨ *STARS:* ${repoInfo.stars}
+🧧 *FORKS:* ${repoInfo.forks}
+📅 *RELEASED:* ${releaseDate}
+🕐 *LAST UPDATED:* ${lastUpdateDate}
+👨‍💻 *CRETOR:* *France King®🐐🔵*`;
+
+
+await zk.sendMessage(dest, { image: { url: img }, caption: gitdata });
+
+} else {
+console.log("Could not fetch data")
+
+}
+
+
+});
+
+   /* let z = 'Hello This is  *FLASH-MD* \n\n ' + "The Following is *FLASH-MD Repo.*";
     let d = ' https://github.com/franceking1/Flash-Md';
     let varmess = z + d;
     var img = 'https://telegra.ph/file/f1f2303ff4e39b0a3b6b3.jpg';
     await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
     //console.log("montest")
 });
-console.log("mon test");
-/*module.exports.commande = () => {
-  var nomCom = ["test","t"]
-  var reaction="☺️"
-  return { nomCom, execute,reaction }
-};
-
-async function  execute  (origineMessage,zok) {
-  console.log("Commande saisie !!!s")
-   let z ='Salut je m\'appelle *FLASH-MD* \n\n '+'je suis un bot Whatsapp Multi-appareil '
-      let d =' developpé par *France King*'
-      let varmess=z+d
-      var img='https://telegra.ph/file/13d63c21c1a665bfd8324.jpg'
-await  zok.sendMessage(origineMessage,  { image:{url:img},caption:varmess});
-}  */ 
+console.log("mon test");*/
