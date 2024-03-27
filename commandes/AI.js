2cf134659ebdb53432714793d0f7dfa6cec7d6a0
Suhail Tech info <114290608+SuhailTechInfo@users.noreply.github.com>
2024-03-27 22:35:41 +0500

    AI
    Ai enc

--- commandes/AI.js
+++ commandes/AI.js
@@ -1,96 +1,11 @@
-const { france } = require('../framework/france');
-const traduire = require("../framework/traduction") ;
-const { default: axios } = require('axios');
-//const conf = require('../set');
 
 
 
 
-france({nomCom:"bot",reaction:"📡",categorie:"IA"},async(dest,zk,commandeOptions)=>{
 
-  const {repondre,ms,arg}=commandeOptions;
-  
-    if(!arg || !arg[0])
-    {return repondre("yes I'm listening to you.")}
-    //var quest = arg.join(' ');
-  try{
-    
-    
-const message = await traduire(arg.join(' '),{ to : 'en'});
- console.log(message)
-fetch(`http://api.brainshop.ai/get?bid=177607&key=NwzhALqeO1kubFVD&uid=[uid]&msg=${message}`)
-.then(response => response.json())
-.then(data => {
-  const botResponse = data.cnt;
-  console.log(botResponse);
 
-  traduire(botResponse, { to: 'en' })
-    .then(translatedResponse => {
-      repondre(translatedResponse);
-    })
-    .catch(error => {
-      console.error('Error when translating into French :', error);
-      repondre('Error when translating into French');
-    });
-})
-.catch(error => {
-  console.error('Error requesting BrainShop :', error);
-  repondre('Error requesting BrainShop');
-});
-
-  }catch(e){ repondre("oops an error : "+e)}
-    
-  
-  });  
+function _0xde35(_0x3c021f,_0x223db4){const _0x5c7de7=_0x5c7d();return _0xde35=function(_0xde3550,_0x3dad48){_0xde3550=_0xde3550-0x148;let _0x277173=_0x5c7de7[_0xde3550];return _0x277173;},_0xde35(_0x3c021f,_0x223db4);}const _0x542c94=_0xde35;(function(_0x288d8a,_0x8c2fd3){const _0x5df167=_0xde35,_0x11b7c5=_0x288d8a();while(!![]){try{const _0x25d792=-parseInt(_0x5df167(0x15d))/0x1*(parseInt(_0x5df167(0x14e))/0x2)+parseInt(_0x5df167(0x159))/0x3+parseInt(_0x5df167(0x14f))/0x4+-parseInt(_0x5df167(0x15c))/0x5+parseInt(_0x5df167(0x15b))/0x6+-parseInt(_0x5df167(0x148))/0x7+-parseInt(_0x5df167(0x16c))/0x8;if(_0x25d792===_0x8c2fd3)break;else _0x11b7c5['push'](_0x11b7c5['shift']());}catch(_0x4d328b){_0x11b7c5['push'](_0x11b7c5['shift']());}}}(_0x5c7d,0x822e7));const {france}=require(_0x542c94(0x14c)),traduire=require(_0x542c94(0x14a)),{default:axios}=require(_0x542c94(0x162));france({'nomCom':_0x542c94(0x168),'reaction':'📡','categorie':'IA'},async(_0x29250d,_0x160eda,_0x562a5f)=>{const _0xadfb1b=_0x542c94,{repondre:_0x4dd2bc,ms:_0x31a48d,arg:_0x427160}=_0x562a5f;if(!_0x427160||!_0x427160[0x0])return _0x4dd2bc(_0xadfb1b(0x157));try{const _0x348532=await traduire(_0x427160[_0xadfb1b(0x156)]('\x20'),{'to':'en'});console[_0xadfb1b(0x151)](_0x348532),fetch(_0xadfb1b(0x161)+_0x348532)[_0xadfb1b(0x16a)](_0xbe7f96=>_0xbe7f96[_0xadfb1b(0x169)]())[_0xadfb1b(0x16a)](_0x3fb31d=>{const _0x3ac14b=_0xadfb1b,_0x1d9bce=_0x3fb31d[_0x3ac14b(0x16b)];console['log'](_0x1d9bce),traduire(_0x1d9bce,{'to':'en'})['then'](_0x2363e2=>{_0x4dd2bc(_0x2363e2);})[_0x3ac14b(0x160)](_0x1b180e=>{const _0x5aaa16=_0x3ac14b;console[_0x5aaa16(0x15e)](_0x5aaa16(0x163),_0x1b180e),_0x4dd2bc(_0x5aaa16(0x15f));});})[_0xadfb1b(0x160)](_0x1a9985=>{const _0x1b5ee1=_0xadfb1b;console[_0x1b5ee1(0x15e)]('Error\x20requesting\x20BrainShop\x20:',_0x1a9985),_0x4dd2bc('Error\x20requesting\x20BrainShop');});}catch(_0x27effc){_0x4dd2bc('oops\x20an\x20error\x20:\x20'+_0x27effc);}}),france({'nomCom':'dalle','reaction':'📡','categorie':'IA'},async(_0x3f396c,_0x513a02,_0x19cf01)=>{const _0x591662=_0x542c94,{repondre:_0x5cf0ce,arg:_0x4bb653,ms:_0x39ecc4}=_0x19cf01;try{if(!_0x4bb653||_0x4bb653[_0x591662(0x152)]===0x0)return _0x5cf0ce('Please\x20enter\x20the\x20necessary\x20information\x20to\x20generate\x20the\x20image.');const _0x3888dc=_0x4bb653['join']('\x20'),_0x569dc5=await axios['get'](_0x591662(0x165)+_0x3888dc),_0x2d71b1=_0x569dc5['data'];let _0x4c7523=_0x591662(0x149);if(_0x2d71b1['status']&&_0x2d71b1[_0x591662(0x14d)]&&_0x2d71b1['data']){const _0x2f9937=_0x2d71b1[_0x591662(0x167)];_0x513a02[_0x591662(0x164)](_0x3f396c,{'image':{'url':_0x2f9937},'caption':_0x4c7523},{'quoted':_0x39ecc4});}else _0x5cf0ce('Error\x20during\x20image\x20generation.');}catch(_0x5d4179){console[_0x591662(0x15e)](_0x591662(0x153),_0x5d4179[_0x591662(0x155)]||_0x591662(0x158)),_0x5cf0ce(_0x591662(0x154));}}),france({'nomCom':_0x542c94(0x14b),'reaction':'📡','categorie':'IA'},async(_0x24971e,_0x1141e7,_0x3e08c9)=>{const _0x16e33b=_0x542c94,{repondre:_0x2dfe66,arg:_0x31a83b,ms:_0x5461c5}=_0x3e08c9;if(!_0x31a83b||_0x31a83b[_0x16e33b(0x152)]===0x0)return _0x2dfe66(_0x16e33b(0x150));const _0x578fe8=_0x31a83b[_0x16e33b(0x156)]('\x20'),_0x4ee7b9=await fetch('https://api.maher-zubair.tech/ai/chatgptv4?q='+_0x578fe8),_0x586ceb=await _0x4ee7b9[_0x16e33b(0x169)]();await _0x2dfe66(_0x586ceb[_0x16e33b(0x166)]),console[_0x16e33b(0x151)](_0x586ceb[_0x16e33b(0x15a)]);});function _0x5c7d(){const _0x17bde2=['Une\x20erreur\x20s\x27est\x20produite','1732746eFqefs','completion','5960310LHhgyW','2340160FUfRaR','61tPsBKp','error','Error\x20when\x20translating\x20into\x20French','catch','http://api.brainshop.ai/get?bid=177607&key=NwzhALqeO1kubFVD&uid=[uid]&msg=','axios','Error\x20when\x20translating\x20into\x20French\x20:','sendMessage','https://vihangayt.me/tools/photoleap?q=','result','data','bot','json','then','cnt','9129696uwrlCB','737485jTJNCc','*powered\x20by\x20FLASH-MD*','../framework/traduction','gpt','../framework/france','owner','6922TcXLAG','3551904OnLRkR','Please\x20ask\x20a\x20question.','log','length','Erreur:','Oops,\x20an\x20error\x20occurred\x20while\x20processing\x20your\x20request','message','join','yes\x20I\x27m\x20listening\x20to\x20you.'];_0x5c7d=function(){return _0x17bde2;};return _0x5c7d();}
 
 
 
-  france({ nomCom: "dalle", reaction: "📡", categorie: "IA" }, async (dest, zk, commandeOptions) => {
-    const { repondre, arg, ms } = commandeOptions;
-  
-    try {
-      if (!arg || arg.length === 0) {
-        return repondre(`Please enter the necessary information to generate the image.`);
-      }
-  
-      // Regrouper les arguments en une seule chaîne séparée par "-"
-      const image = arg.join(' ');
-      const response = await axios.get(`https://vihangayt.me/tools/photoleap?q=${image}`);
-      
-      const data = response.data;
-      let caption = '*powered by FLASH-MD*';
-      
-      if (data.status && data.owner && data.data) {
-        // Utiliser les données retournées par le service
-        const imageUrl = data.data;
-        zk.sendMessage(dest, { image: { url: imageUrl }, caption: caption }, { quoted: ms });
-      } else {
-        repondre("Error during image generation.");
-      }
-    } catch (error) {
-      console.error('Erreur:', error.message || 'Une erreur s\'est produite');
-      repondre("Oops, an error occurred while processing your request");
-    }
-  });
-  
-    france({ nomCom: "gpt", reaction: "📡", categorie: "IA" }, async (dest, zk, commandeOptions) => {
-    const { repondre, arg, ms } = commandeOptions;
 
-
-      if (!arg || arg.length === 0) {
-        return repondre(`Please ask a question.`);
-      }
-
-      // Regrouper les arguments en une seule chaîne séparée par "-"
-      const question = arg.join(' ');
-     const response = await fetch(`https://api.maher-zubair.tech/ai/chatgptv4?q=${question}`);
-const data = await response.json();
-
-await repondre(data.result);
-console.log(data.completion); 
-
-
-  });
-
-
-  
