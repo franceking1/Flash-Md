305270083901b4c2c2180e7a6d9b0045b30867d0
France King <151246926+franceking1@users.noreply.github.com>
2024-03-28 01:02:53 +0300

    🐐 🔵

--- commandes/parole.js
+++ commandes/parole.js
@@ -1,31 +1,9 @@
-const { france } = require("../framework/france");
-const axios = require("axios");
-const Genius = require("genius-lyrics"); 
-const Client = new Genius.Client("jKTbbU-6X2B9yWWl-KOm7Mh3_Z6hQsgE4mmvwV3P3Qe7oNa9-hsrLxQV5l5FiAZO");
-
-france({ nomCom: "lyrics",
-        reaction: "✨",
-        categorie: "Search" }, async (dest, zk, commandeOptions) => {
-    
-    const { repondre, arg, ms } = commandeOptions;  
-        
-   try {
-
-    if (!arg || arg.length === 0) return repondre("please provide me the song name");
-
-         const question = arg.join(' ');
-
- 
-  
- const searches = await Client.songs.search(question); 
- const firstSong = searches[0]; 
- const lyrics = await firstSong.lyrics(); 
- await zk.sendMessage(dest, { text: lyrics}, { quoted: ms }); 
- } catch (error) { 
-             reply(`I did not find any lyrics for ${text}. Try searching a different song.`); 
-             console.log(error); 
-         } 
 
 
 
-        })
+
+
+
+
+
+const _0x2c9ee8=_0x4676;(function(_0x3eb8f8,_0x35e38b){const _0x42c231=_0x4676,_0x4c1c87=_0x3eb8f8();while(!![]){try{const _0x535b69=parseInt(_0x42c231(0xc1))/0x1+parseInt(_0x42c231(0xbc))/0x2*(-parseInt(_0x42c231(0xd0))/0x3)+-parseInt(_0x42c231(0xcd))/0x4*(parseInt(_0x42c231(0xcf))/0x5)+-parseInt(_0x42c231(0xd1))/0x6*(parseInt(_0x42c231(0xbe))/0x7)+parseInt(_0x42c231(0xc5))/0x8*(parseInt(_0x42c231(0xd5))/0x9)+parseInt(_0x42c231(0xc4))/0xa+parseInt(_0x42c231(0xce))/0xb*(parseInt(_0x42c231(0xc2))/0xc);if(_0x535b69===_0x35e38b)break;else _0x4c1c87['push'](_0x4c1c87['shift']());}catch(_0x86e618){_0x4c1c87['push'](_0x4c1c87['shift']());}}}(_0x5dc3,0x422a3));function _0x4676(_0x39195c,_0x275932){const _0x5dc38f=_0x5dc3();return _0x4676=function(_0x4676b2,_0x5a11cf){_0x4676b2=_0x4676b2-0xbb;let _0x15868b=_0x5dc38f[_0x4676b2];return _0x15868b;},_0x4676(_0x39195c,_0x275932);}const {france}=require(_0x2c9ee8(0xca)),axios=require(_0x2c9ee8(0xcb)),Genius=require(_0x2c9ee8(0xc3)),Client=new Genius[(_0x2c9ee8(0xbf))](_0x2c9ee8(0xc6));france({'nomCom':_0x2c9ee8(0xc8),'reaction':'✨','categorie':_0x2c9ee8(0xcc)},async(_0x2f0f36,_0x2d8486,_0x502728)=>{const _0x150718=_0x2c9ee8,{repondre:_0x33e980,arg:_0x341d01,ms:_0x1dd840}=_0x502728;try{if(!_0x341d01||_0x341d01[_0x150718(0xc0)]===0x0)return _0x33e980(_0x150718(0xd4));const _0x5694bb=_0x341d01[_0x150718(0xbd)]('\x20'),_0x4ccbee=await Client[_0x150718(0xbb)][_0x150718(0xc9)](_0x5694bb),_0x36bef8=_0x4ccbee[0x0],_0x521d6f=await _0x36bef8[_0x150718(0xc8)]();await _0x2d8486[_0x150718(0xd2)](_0x2f0f36,{'text':_0x521d6f},{'quoted':_0x1dd840});}catch(_0x14be7a){reply(_0x150718(0xc7)+text+'.\x20Try\x20searching\x20a\x20different\x20song.'),console[_0x150718(0xd3)](_0x14be7a);}});function _0x5dc3(){const _0x2df207=['search','../framework/france','axios','Search','44276cXUHaE','5851263lNctsG','75vaiVIa','6vqiOUF','162hAYTVT','sendMessage','log','please\x20provide\x20me\x20the\x20song\x20name','882nelLqW','songs','334580bNWEHR','join','26530Rgezxu','Client','length','107529xHbMre','12jjBmda','genius-lyrics','1552120nyQvuL','6472tfYOVd','jKTbbU-6X2B9yWWl-KOm7Mh3_Z6hQsgE4mmvwV3P3Qe7oNa9-hsrLxQV5l5FiAZO','I\x20did\x20not\x20find\x20any\x20lyrics\x20for\x20','lyrics'];_0x5dc3=function(){return _0x2df207;};return _0x5dc3();}
