3317e5e58c3e6afc7e43a5e922dd96a2728b6013
France King <151246926+franceking1@users.noreply.github.com>
2024-03-28 01:06:13 +0300

    🐐 🔵

--- commandes/test.js
+++ commandes/test.js
@@ -1,51 +1,5 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const { france } = require("../framework/france");
-france({ nomCom: "repo", reaction: "😌", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
 
 
-const githubRepo = 'https://api.github.com/repos/franceking1/Flash-Md';
-const img = 'https://telegra.ph/file/f1f2303ff4e39b0a3b6b3.jpg';
 
 
-    const response = await fetch(githubRepo); 
-        const data = await response.json(); 
-
-        if (data) {
-            const repoInfo = {
-                stars: data.stargazers_count,
-                forks: data.forks_count,
-                lastUpdate: data.updated_at,
-                owner: data.owner.login
-            };
-const releaseDate = new Date(data.created_at).toLocaleDateString('en-GB');
-            const lastUpdateDate = new Date(repoInfo.lastUpdate).toLocaleDateString('en-GB');
-
-const gitdata = `Hello 👋 
-This is *FLASH-MD.* The following is *FLASH-MD's*
-*REPOSITORY:* ${data.html_url}
-✨ *STARS:* ${repoInfo.stars}
-🧧 *FORKS:* ${repoInfo.forks}
-📅 *RELEASED:* ${releaseDate}
-🕐 *LAST UPDATED:* ${lastUpdateDate}
-👨‍💻 *OWNER:* *France King®🐐🔵*`;
-
-
-await zk.sendMessage(dest, { image: { url: img }, caption: gitdata });
-
-} else {
-console.log("Could not fetch data")
-
-}
-
-
-});
-
-   /* let z = 'Hello This is  *FLASH-MD* \n\n ' + "The Following is *FLASH-MD Repo.*";
-    let d = ' https://github.com/franceking1/Flash-Md';
-    let varmess = z + d;
-    var img = 'https://telegra.ph/file/f1f2303ff4e39b0a3b6b3.jpg';
-    await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
-    //console.log("montest")
-});
-console.log("mon test");*/
+'use strict';function _0x239a(_0x3550cb,_0x1e8b4f){const _0x51c9d2=_0x51c9();return _0x239a=function(_0x239a63,_0x281b27){_0x239a63=_0x239a63-0x11d;let _0x1ef610=_0x51c9d2[_0x239a63];return _0x1ef610;},_0x239a(_0x3550cb,_0x1e8b4f);}const _0x25b904=_0x239a;(function(_0x1fb147,_0xdd9f5){const _0x470cd6=_0x239a,_0x17b1e4=_0x1fb147();while(!![]){try{const _0x2e0a84=-parseInt(_0x470cd6(0x135))/0x1*(-parseInt(_0x470cd6(0x122))/0x2)+-parseInt(_0x470cd6(0x132))/0x3+-parseInt(_0x470cd6(0x136))/0x4*(parseInt(_0x470cd6(0x126))/0x5)+parseInt(_0x470cd6(0x12c))/0x6+parseInt(_0x470cd6(0x137))/0x7*(parseInt(_0x470cd6(0x12a))/0x8)+parseInt(_0x470cd6(0x121))/0x9+-parseInt(_0x470cd6(0x125))/0xa;if(_0x2e0a84===_0xdd9f5)break;else _0x17b1e4['push'](_0x17b1e4['shift']());}catch(_0x398dd8){_0x17b1e4['push'](_0x17b1e4['shift']());}}}(_0x51c9,0xb1dc4));Object[_0x25b904(0x129)](exports,_0x25b904(0x12b),{'value':!![]});const {france}=require(_0x25b904(0x13a));function _0x51c9(){const _0x8491ed=['3qcKvmk','408fLvdwc','16891CXqFrE','Could\x20not\x20fetch\x20data','toLocaleDateString','../framework/france','\x0a📅\x20*RELEASED:*\x20','forks','en-GB','created_at','log','8644500kBYkvy','699018dydKhE','stargazers_count','sendMessage','24546530ZePwrl','10345qMsNJS','repo','html_url','defineProperty','3376zXtHZr','__esModule','7327548XOtJNn','\x0a👨‍💻\x20*OWNER:*\x20*France\x20King®🐐🔵*','updated_at','Hello\x20👋\x20\x0aThis\x20is\x20*FLASH-MD.*\x20The\x20following\x20is\x20*FLASH-MD\x27s*\x0a*REPOSITORY:*\x20','json','\x0a🕐\x20*LAST\x20UPDATED:*\x20','2563092YPziZZ','owner','https://telegra.ph/file/f1f2303ff4e39b0a3b6b3.jpg'];_0x51c9=function(){return _0x8491ed;};return _0x51c9();}france({'nomCom':_0x25b904(0x127),'reaction':'😌','nomFichier':__filename},async(_0x8b867f,_0xe75a3c,_0x6741a1)=>{const _0x55c985=_0x25b904,_0x2c593a='https://api.github.com/repos/franceking1/Flash-Md',_0x4469bc=_0x55c985(0x134),_0x953bd2=await fetch(_0x2c593a),_0x57babb=await _0x953bd2[_0x55c985(0x130)]();if(_0x57babb){const _0x5a4b29={'stars':_0x57babb[_0x55c985(0x123)],'forks':_0x57babb['forks_count'],'lastUpdate':_0x57babb[_0x55c985(0x12e)],'owner':_0x57babb[_0x55c985(0x133)]['login']},_0x4437fa=new Date(_0x57babb[_0x55c985(0x11f)])['toLocaleDateString'](_0x55c985(0x11e)),_0x236fc9=new Date(_0x5a4b29['lastUpdate'])[_0x55c985(0x139)](_0x55c985(0x11e)),_0xb98bb7=_0x55c985(0x12f)+_0x57babb[_0x55c985(0x128)]+'\x0a✨\x20*STARS:*\x20'+_0x5a4b29['stars']+'\x0a🧧\x20*FORKS:*\x20'+_0x5a4b29[_0x55c985(0x11d)]+_0x55c985(0x13b)+_0x4437fa+_0x55c985(0x131)+_0x236fc9+_0x55c985(0x12d);await _0xe75a3c[_0x55c985(0x124)](_0x8b867f,{'image':{'url':_0x4469bc},'caption':_0xb98bb7});}else console[_0x55c985(0x120)](_0x55c985(0x138));});
