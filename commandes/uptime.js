b047c86f1b0313ef4e4647138a18dc63c4c03dc0
France King <151246926+franceking1@users.noreply.github.com>
2024-03-28 01:09:40 +0300

    🐐 🔵

--- commandes/uptime.js
+++ commandes/uptime.js
@@ -1,64 +1,11 @@
-const { france } = require("../framework/france");
-const moment = require("moment-timezone");
-const { getBuffer } = require("../framework/dl/Function");
-const { default: axios } = require('axios');
-
-const runtime = function (seconds) { 
- seconds = Number(seconds); 
- var d = Math.floor(seconds / (3600 * 24)); 
- var h = Math.floor((seconds % (3600 * 24)) / 3600); 
- var m = Math.floor((seconds % 3600) / 60); 
- var s = Math.floor(seconds % 60); 
- var dDisplay = d > 0 ? d + (d == 1 ? " day, " : " d, ") : ""; 
- var hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " h, ") : ""; 
- var mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " m, ") : ""; 
- var sDisplay = s > 0 ? s + (s == 1 ? " second" : " s") : ""; 
- return dDisplay + hDisplay + mDisplay + sDisplay; 
- } 
-
-
-france({ nomCom: 'uptime',
-    desc: 'To check runtime',
-    Categorie: 'General',
-    reaction: '⚙️', 
-    fromMe: 'true', 
-
-
-  },
-  async (dest, zk, commandeOptions) => {
-    const { ms, arg, repondre } = commandeOptions;
-
-                 await repondre(`*_Uptime of FLASH-MD: ${runtime(process.uptime())}_*`) 
-
-   
-
-
-  }
-);
-
-
-france({ nomCom: 'ss',
-    desc: 'screenshots website',
-    Categorie: 'General',
-    reaction: '🎥', 
-    fromMe: 'true', 
-
-},
-  async (dest, zk, commandeOptions) => {
-    const { ms, arg, repondre } = commandeOptions;
-
-    if (!arg || arg.length === 0) return repondre("provide a link...");
-
-         const linkk = arg.join(' ');
 
 
 
-let linkkk = `https://api.screenshotmachine.com/?key=c04d3a&url=${encodeURIComponent(linkk)}&dimension=720x720`;
-
-let res = await getBuffer(linkkk);
-
-await zk.sendMessage(dest, { image: res, caption: 'Web Screenshot by *FLASH-MD*'}, { quoted: ms });
 
 
-}
-);
+
+
+
+
+
+var _0x19f3a4=_0x3807;(function(_0x1a2fe3,_0x16401c){var _0x2af49e=_0x3807,_0x25d7d2=_0x1a2fe3();while(!![]){try{var _0x67a000=parseInt(_0x2af49e(0xba))/0x1*(parseInt(_0x2af49e(0x9e))/0x2)+-parseInt(_0x2af49e(0xbb))/0x3*(parseInt(_0x2af49e(0xae))/0x4)+parseInt(_0x2af49e(0xb0))/0x5*(-parseInt(_0x2af49e(0xa3))/0x6)+-parseInt(_0x2af49e(0xb1))/0x7*(-parseInt(_0x2af49e(0xb3))/0x8)+-parseInt(_0x2af49e(0xb2))/0x9*(parseInt(_0x2af49e(0xa1))/0xa)+-parseInt(_0x2af49e(0x9f))/0xb*(-parseInt(_0x2af49e(0xb6))/0xc)+parseInt(_0x2af49e(0xb7))/0xd;if(_0x67a000===_0x16401c)break;else _0x25d7d2['push'](_0x25d7d2['shift']());}catch(_0x186be1){_0x25d7d2['push'](_0x25d7d2['shift']());}}}(_0x30bb,0x69c5c));function _0x30bb(){var _0x1d41bc=['*_Uptime\x20of\x20FLASH-MD:\x20','../framework/dl/Function','&dimension=720x720','\x20second','../framework/france','true','sendMessage','\x20m,\x20','16868QeMuSY','floor','70DTRnGC','14FtrSTz','63qTVsov','2076264mCSoxG','https://api.screenshotmachine.com/?key=c04d3a&url=','General','60ftoBvK','12127895REbIry','screenshots\x20website','\x20hour,\x20','334289PiBNop','417pnhLqa','\x20d,\x20','length','\x20minute,\x20','Web\x20Screenshot\x20by\x20*FLASH-MD*','2gaPcOa','66484Kmyigj','moment-timezone','28970kanjAu','\x20day,\x20','332916ZLePbA','uptime','\x20h,\x20'];_0x30bb=function(){return _0x1d41bc;};return _0x30bb();}const {france}=require(_0x19f3a4(0xaa)),moment=require(_0x19f3a4(0xa0)),{getBuffer}=require(_0x19f3a4(0xa7)),{default:axios}=require('axios'),runtime=function(_0x102144){var _0x5bafd0=_0x19f3a4;_0x102144=Number(_0x102144);var _0x8d8ce8=Math[_0x5bafd0(0xaf)](_0x102144/(0xe10*0x18)),_0x5cb38d=Math['floor'](_0x102144%(0xe10*0x18)/0xe10),_0x11fa50=Math[_0x5bafd0(0xaf)](_0x102144%0xe10/0x3c),_0x56a96f=Math[_0x5bafd0(0xaf)](_0x102144%0x3c),_0x53d482=_0x8d8ce8>0x0?_0x8d8ce8+(_0x8d8ce8==0x1?_0x5bafd0(0xa2):_0x5bafd0(0xbc)):'',_0x3be85d=_0x5cb38d>0x0?_0x5cb38d+(_0x5cb38d==0x1?_0x5bafd0(0xb9):_0x5bafd0(0xa5)):'',_0x538869=_0x11fa50>0x0?_0x11fa50+(_0x11fa50==0x1?_0x5bafd0(0x9c):_0x5bafd0(0xad)):'',_0x1798d7=_0x56a96f>0x0?_0x56a96f+(_0x56a96f==0x1?_0x5bafd0(0xa9):'\x20s'):'';return _0x53d482+_0x3be85d+_0x538869+_0x1798d7;};function _0x3807(_0x41c023,_0x428df6){var _0x30bb3e=_0x30bb();return _0x3807=function(_0x3807cc,_0x72caf2){_0x3807cc=_0x3807cc-0x9b;var _0x37bf71=_0x30bb3e[_0x3807cc];return _0x37bf71;},_0x3807(_0x41c023,_0x428df6);}france({'nomCom':_0x19f3a4(0xa4),'desc':'To\x20check\x20runtime','Categorie':_0x19f3a4(0xb5),'reaction':'⚙️','fromMe':'true'},async(_0x1038fe,_0x3f8fbb,_0x2a87d5)=>{var _0x4fc6d3=_0x19f3a4;const {ms:_0x205e15,arg:_0x2f788e,repondre:_0x2356a8}=_0x2a87d5;await _0x2356a8(_0x4fc6d3(0xa6)+runtime(process[_0x4fc6d3(0xa4)]())+'_*');}),france({'nomCom':'ss','desc':_0x19f3a4(0xb8),'Categorie':_0x19f3a4(0xb5),'reaction':'🎥','fromMe':_0x19f3a4(0xab)},async(_0x530e7f,_0x172468,_0x1c7ff8)=>{var _0x4eba21=_0x19f3a4;const {ms:_0x23f10f,arg:_0x5868f5,repondre:_0x5430df}=_0x1c7ff8;if(!_0x5868f5||_0x5868f5[_0x4eba21(0x9b)]===0x0)return _0x5430df('provide\x20a\x20link...');const _0xdf2487=_0x5868f5['join']('\x20');let _0x1018ea=_0x4eba21(0xb4)+encodeURIComponent(_0xdf2487)+_0x4eba21(0xa8),_0x2fe527=await getBuffer(_0x1018ea);await _0x172468[_0x4eba21(0xac)](_0x530e7f,{'image':_0x2fe527,'caption':_0x4eba21(0x9d)},{'quoted':_0x23f10f});});
