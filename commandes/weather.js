5fa49a28cebd175d983244394bf3d840ac8ed7f4
King-fabian <164608464+King-fabian@users.noreply.github.com>
2024-03-30 11:42:27 +0300

    🐐 🔵

--- commandes/weather.js
+++ commandes/weather.js
@@ -3,32 +3,49 @@ const fs = require('fs');
 const {zokou} = require('../framework/zokou');
 const { writeFile } = require('fs/promises')
 
-zokou({nomCom : "weather" , categorie : "NEW"},async (dest , zk , commandeOptions)=>{
-  const {ms,repondre,arg} = commandeOptions ;
-if (!text) return repondre("Give me location!!");
-            const response = await axios.get(
-                `https://api.openweathermap.org/data/2.5/weather?q=${text}&units=metric&appid=060a6bcfa19809c2cd4d97a212b19273&language=en`
-            );
-            let textw = "";
-            textw += `*🌟Weather of  ${text}*\n\n`;
-            textw += `*Weather:-* ${wdata.data.weather[0].main}\n`;
-            textw += `*Description:-* ${wdata.data.weather[0].description}\n`;
-            textw += `*Avg Temp:-* ${wdata.data.main.temp}\n`;
-            textw += `*Feels Like:-* ${wdata.data.main.feels_like}\n`;
-            textw += `*Pressure:-* ${wdata.data.main.pressure}\n`;
-            textw += `*Humidity:-* ${wdata.data.main.humidity}\n`;
-            textw += `*Humidity:-* ${wdata.data.wind.speed}\n`;
-            textw += `*Latitude:-* ${wdata.data.coord.lat}\n`;
-            textw += `*Longitude:-* ${wdata.data.coord.lon}\n`;
-            textw += `*Country:-* ${wdata.data.sys.country}\n`;
+zokou({ nomCom: "weather",
+        reaction: "🌡️",
+        categorie: "Search" }, async (dest, zk, commandeOptions) => {
 
-            dest.sendMessage(
-                zk.chat, {
-                    text: textw,
-                }, {
-                    quoted: zk,
-                }
-            );
+    const { repondre, arg, ms } = commandeOptions;  
+      const question = arg.join(' ');
+if (!question) return repondre("Give me location...");
 
-        }
-    )
+            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${question}&units=metric&appid=060a6bcfa19809c2cd4d97a212b19273&language=en`);
+
+        const data = await response.json();
+
+
+        const cityName = data.name;
+        const temperature = data.main.temp;
+        const feelsLike = data.main.feels_like;
+        const minTemperature = data.main.temp_min;
+        const maxTemperature = data.main.temp_max;
+        const description = data.weather[0].description;
+        const humidity = data.main.humidity;
+        const windSpeed = data.wind.speed;
+        const rainVolume = data.rain ? data.rain['1h'] : 0;
+        const cloudiness = data.clouds.all;
+        const sunrise = new Date(data.sys.sunrise * 1000);
+        const sunset = new Date(data.sys.sunset * 1000);
+        const country = (data.sy.conutry);
+       
+
+await repondre(`❄️ Weather in ${cityName}
+
+🌡️ Temperature: ${temperature}°C
+📝 Description: ${description}
+❄️ Humidity: ${humidity}%
+🌀 Wind Speed: ${windSpeed} m/s
+🌧️ Rain Volume (last hour): ${rainVolume} mm
+☁️ Cloudiness: ${cloudiness}%
+🌄 Sunrise: ${sunrise.toLocaleTimeString()}
+🌅 Sunset: ${sunset.toLocaleTimeString()}
+💫country: ${country}`);
+
+
+
+
+            
+
+        });
