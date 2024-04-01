0f99750f33cd882e858f82efdc259a20ab1722b2
King-fabian <164608464+King-fabian@users.noreply.github.com>
2024-04-01 20:26:16 +0300

    🐐 🔵

--- commandes/menu.js
+++ commandes/menu.js
@@ -50,7 +50,7 @@ const date = moment().format('DD/MM/YYYY');
 `;
 
     for (const cat in coms) {
-        menuMsg += `*╭────☢️* *${cat}* *☢️⊷*`;
+        menuMsg += `*╭────☢️${cat}☢️⊷*`;
         for (const cmd of coms[cat]) {
             menuMsg += `
  *|☬* ${cmd}`;
