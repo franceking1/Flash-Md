0dfa2cf18eaaac4ee7452cd10e344db4e1215529
France King <151246926+franceking1@users.noreply.github.com>
2024-04-06 16:08:52 +0300

    🐐 🔵

--- commandes/quote.js
+++ commandes/quote.js
@@ -1,6 +1,6 @@
 const { france } = require('../framework/france');
 
-france({ nomCom: 'quote', categorie: 'Fun' }, async (dest, zk, commandeOptions) => {
+france({ nomCom: 'quote', categorie: 'Group' }, async (dest, zk, commandeOptions) => {
   const { ms, repondre, verifGroupe, arg } = commandeOptions;
   if (!verifGroupe) {
     repondre('Commande réservée au groupe uniquement');
@@ -12,9 +12,7 @@ france({ nomCom: 'quote', categorie: 'Fun' }, async (dest, zk, commandeOptions) 
       fetch('https://animechan.xyz/api/random')
         .then((response) => response.json())
         .then(async (quote) => {
-          repondre(`╔══════════════════════════╗
-║   FLASH-MD              ║
-╚══════════════════════════╝
+          repondre(`*FLASH-MD Quotes*
 
 🎬 Anime: ${quote.anime}
 👤 Character: ${quote.character}
@@ -32,15 +30,13 @@ Powered by *France King*`);
       fetch('https://animechan.xyz/api/random/character?name=' + query)
         .then((response) => response.json())
         .then(async (quote) => {
-          repondre(`╔══════════════════════════╗
-║   FLASH-MD               ║
-╚══════════════════════════╝
+          repondre(`FLASH-MD
 
 🎬 Anime: ${quote.anime}
 👤 Character: ${quote.character}
 💬 Quote: ${quote.quote}
 
-Powered by FLASH-MD`);
+Powered by *FLASH-MD*`);
         });
     } catch (e) {
       repondre('Erreur lors de la génération de la citation : ' + e.message);
