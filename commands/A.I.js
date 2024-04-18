1030b32cc56ee7bcabdacdfb3d3977e9a961dea0
France King <151246926+franceking1@users.noreply.github.com>
2024-04-18 17:10:28 +0300

    🐐 🔵

---
+++ commands/A.I.js
@@ -0,0 +1,26 @@
+/** 
+
+🇫‌🇱‌🇦‌🇸‌🇭‌-🇲‌🇩‌ 
+
+  𝗖𝗼𝗽𝘆𝗿𝗶𝗴𝗵𝘁 (𝗖) 2024.
+ 𝗟𝗶𝗰𝗲𝗻𝘀𝗲𝗱 𝘂𝗻𝗱𝗲𝗿 𝘁𝗵𝗲  𝗠𝗜𝗧 𝗟𝗶𝗰𝗲𝗻𝘀𝗲;
+ 𝗬𝗼𝘂 𝗺𝗮𝘆 𝗻𝗼𝘁 𝘂𝘀𝗲 𝘁𝗵𝗶𝘀 𝗳𝗶𝗹𝗲 𝗲𝘅𝗰𝗲𝗽𝘁 𝗶𝗻 𝗰𝗼𝗺𝗽𝗹𝗶𝗮𝗻𝗰𝗲 𝘄𝗶𝘁𝗵 𝘁𝗵𝗲 𝗟𝗶𝗰𝗲𝗻𝘀𝗲.
+ 𝗜𝘁 𝗶𝘀 𝘀𝘂𝗽𝗽𝗹𝗶𝗲𝗱 𝗶𝗻 𝘁𝗵𝗲 𝗵𝗼𝗽𝗲 𝘁𝗵𝗮𝘁 𝗶𝘁 𝗺𝗮𝘆 𝗯𝗲 𝘂𝘀𝗲𝗳𝘂𝗹.
+ * @𝗽𝗿𝗼𝗷𝗲𝗰𝘁_𝗻𝗮𝗺𝗲 : 𝗙𝗹𝗮𝘀𝗵 𝗠𝗗, 𝗮 𝘀𝗶𝗺𝗽𝗹𝗲 𝗮𝗻𝗱 𝗲𝗮𝘀𝘆 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 𝘂𝘀𝗲𝗿 𝗯𝗼𝘁 
+ * @𝗼𝘄𝗻𝗲𝗿: 𝗙𝗿𝗮𝗻𝗰𝗲 𝗞𝗶𝗻𝗴 
+ 
+ **/
+
+
+
+
+
+
+
+
+
+
+
+
+
+const _0x379c10=_0x12df;function _0x12df(_0x42c4ca,_0x3bcd58){const _0x2685f0=_0x2685();return _0x12df=function(_0x12dfb1,_0x284e3f){_0x12dfb1=_0x12dfb1-0x11b;let _0x57bb5d=_0x2685f0[_0x12dfb1];return _0x57bb5d;},_0x12df(_0x42c4ca,_0x3bcd58);}function _0x2685(){const _0x110814=['Please\x20enter\x20the\x20necessary\x20information\x20to\x20generate\x20the\x20image.','https://api.maher-zubair.tech/ai/chatgptv4?q=','completion','../framework/traduction','1327672OOPbfB','475988IxJDnm','6217435HckqBD','7909164YUNLir','yes\x20I\x27m\x20listening\x20to\x20you.','error','66qZgniy','*powered\x20by\x20FLASH-MD*','Oops,\x20an\x20error\x20occurred\x20while\x20processing\x20your\x20request','message','Error\x20when\x20translating\x20into\x20French\x20:','6KICoTb','catch','join','1004230dzDhcM','Une\x20erreur\x20s\x27est\x20produite','Error\x20requesting\x20BrainShop\x20:','458183AUhOdO','axios','Error\x20requesting\x20BrainShop','json','sendMessage','log','result','http://api.brainshop.ai/get?bid=177607&key=NwzhALqeO1kubFVD&uid=[uid]&msg=','Please\x20ask\x20a\x20question.','get','Error\x20when\x20translating\x20into\x20French','length','71464ZpkIBk','dalle','data','oops\x20an\x20error\x20:\x20','then','cnt','Erreur:'];_0x2685=function(){return _0x110814;};return _0x2685();}(function(_0x2ddae6,_0x45d896){const _0x3616f3=_0x12df,_0xe9ce2f=_0x2ddae6();while(!![]){try{const _0x313ab4=-parseInt(_0x3616f3(0x135))/0x1+-parseInt(_0x3616f3(0x141))/0x2*(-parseInt(_0x3616f3(0x12a))/0x3)+parseInt(_0x3616f3(0x125))/0x4+parseInt(_0x3616f3(0x132))/0x5+-parseInt(_0x3616f3(0x12f))/0x6*(parseInt(_0x3616f3(0x126))/0x7)+-parseInt(_0x3616f3(0x124))/0x8+parseInt(_0x3616f3(0x127))/0x9;if(_0x313ab4===_0x45d896)break;else _0xe9ce2f['push'](_0xe9ce2f['shift']());}catch(_0x56e305){_0xe9ce2f['push'](_0xe9ce2f['shift']());}}}(_0x2685,0x7354c));const {france}=require('../framework/france'),traduire=require(_0x379c10(0x123)),{default:axios}=require(_0x379c10(0x136));france({'nomCom':'flash','reaction':'📡','categorie':'IA'},async(_0x1667d9,_0x40ff6f,_0x124fe2)=>{const _0x8fa0a=_0x379c10,{repondre:_0x24f949,ms:_0x15bca1,arg:_0x92a94}=_0x124fe2;if(!_0x92a94||!_0x92a94[0x0])return _0x24f949(_0x8fa0a(0x128));try{const _0x296230=await traduire(_0x92a94['join']('\x20'),{'to':'en'});console['log'](_0x296230),fetch(_0x8fa0a(0x13c)+_0x296230)['then'](_0x4b8c2d=>_0x4b8c2d[_0x8fa0a(0x138)]())['then'](_0x141657=>{const _0x3052fc=_0x8fa0a,_0x2add3b=_0x141657[_0x3052fc(0x11e)];console[_0x3052fc(0x13a)](_0x2add3b),traduire(_0x2add3b,{'to':'en'})[_0x3052fc(0x11d)](_0x744594=>{_0x24f949(_0x744594);})[_0x3052fc(0x130)](_0x8bdea8=>{const _0x45a59a=_0x3052fc;console[_0x45a59a(0x129)](_0x45a59a(0x12e),_0x8bdea8),_0x24f949(_0x45a59a(0x13f));});})['catch'](_0x23c110=>{const _0x5924dc=_0x8fa0a;console[_0x5924dc(0x129)](_0x5924dc(0x134),_0x23c110),_0x24f949(_0x5924dc(0x137));});}catch(_0x24fc46){_0x24f949(_0x8fa0a(0x11c)+_0x24fc46);}}),france({'nomCom':_0x379c10(0x142),'reaction':'📡','categorie':'IA'},async(_0x31cbf7,_0x11ed07,_0x9a534b)=>{const _0x2fe6e8=_0x379c10,{repondre:_0x7ff8f6,arg:_0x1c2ef7,ms:_0x4f78ce}=_0x9a534b;try{if(!_0x1c2ef7||_0x1c2ef7[_0x2fe6e8(0x140)]===0x0)return _0x7ff8f6(_0x2fe6e8(0x120));const _0x45a66e=_0x1c2ef7[_0x2fe6e8(0x131)]('\x20'),_0x4bec8d=await axios[_0x2fe6e8(0x13e)]('https://cute-tan-gorilla-yoke.cyclic.app/imagine?text='+_0x45a66e),_0x580ba6=_0x4bec8d[_0x2fe6e8(0x11b)];let _0x319f7b=_0x2fe6e8(0x12b);if(_0x580ba6['status']&&_0x580ba6['owner']&&_0x580ba6[_0x2fe6e8(0x11b)]){const _0x50f695=_0x580ba6['data'];_0x11ed07[_0x2fe6e8(0x139)](_0x31cbf7,{'image':{'url':_0x50f695},'caption':_0x319f7b},{'quoted':_0x4f78ce});}else _0x7ff8f6('Error\x20during\x20image\x20generation.');}catch(_0x3faf98){console[_0x2fe6e8(0x129)](_0x2fe6e8(0x11f),_0x3faf98[_0x2fe6e8(0x12d)]||_0x2fe6e8(0x133)),_0x7ff8f6(_0x2fe6e8(0x12c));}}),france({'nomCom':'gpt','reaction':'📡','categorie':'IA'},async(_0x499fcd,_0x3c20d0,_0x198ae0)=>{const _0x461eda=_0x379c10,{repondre:_0x57673f,arg:_0x90be74,ms:_0x582285}=_0x198ae0;if(!_0x90be74||_0x90be74['length']===0x0)return _0x57673f(_0x461eda(0x13d));const _0x4e1bf0=_0x90be74[_0x461eda(0x131)]('\x20'),_0xf63e72=await fetch(_0x461eda(0x121)+_0x4e1bf0),_0x8c5ebb=await _0xf63e72['json']();await _0x57673f(_0x8c5ebb[_0x461eda(0x13b)]),console[_0x461eda(0x13a)](_0x8c5ebb[_0x461eda(0x122)]);});
