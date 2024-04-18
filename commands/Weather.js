2754df613c283446388d1bda0958602307d4a451
France King <151246926+franceking1@users.noreply.github.com>
2024-04-18 18:33:40 +0300

    🐐 🔵

---
+++ commands/Weather.js
@@ -0,0 +1,25 @@
+
+/** 
+
+🇫‌🇱‌🇦‌🇸‌🇭‌-🇲‌🇩‌ 
+
+  𝗖𝗼𝗽𝘆𝗿𝗶𝗴𝗵𝘁 (𝗖) 2024.
+ 𝗟𝗶𝗰𝗲𝗻𝘀𝗲𝗱 𝘂𝗻𝗱𝗲𝗿 𝘁𝗵𝗲  𝗠𝗜𝗧 𝗟𝗶𝗰𝗲𝗻𝘀𝗲;
+ 𝗬𝗼𝘂 𝗺𝗮𝘆 𝗻𝗼𝘁 𝘂𝘀𝗲 𝘁𝗵𝗶𝘀 𝗳𝗶𝗹𝗲 𝗲𝘅𝗰𝗲𝗽𝘁 𝗶𝗻 𝗰𝗼𝗺𝗽𝗹𝗶𝗮𝗻𝗰𝗲 𝘄𝗶𝘁𝗵 𝘁𝗵𝗲 𝗟𝗶𝗰𝗲𝗻𝘀𝗲.
+ 𝗜𝘁 𝗶𝘀 𝘀𝘂𝗽𝗽𝗹𝗶𝗲𝗱 𝗶𝗻 𝘁𝗵𝗲 𝗵𝗼𝗽𝗲 𝘁𝗵𝗮𝘁 𝗶𝘁 𝗺𝗮𝘆 𝗯𝗲 𝘂𝘀𝗲𝗳𝘂𝗹.
+ * @𝗽𝗿𝗼𝗷𝗲𝗰𝘁_𝗻𝗮𝗺𝗲 : 𝗙𝗹𝗮𝘀𝗵 𝗠𝗗, 𝗮 𝘀𝗶𝗺𝗽𝗹𝗲 𝗮𝗻𝗱 𝗲𝗮𝘀𝘆 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 𝘂𝘀𝗲𝗿 𝗯𝗼𝘁 
+ * @𝗼𝘄𝗻𝗲𝗿: 𝗙𝗿𝗮𝗻𝗰𝗲 𝗞𝗶𝗻𝗴 
+ 
+ **/
+
+
+
+
+
+
+
+
+
+
+
+const _0x53daae=_0x52e8;function _0x52e8(_0x36594a,_0xdc033d){const _0x322d4a=_0x322d();return _0x52e8=function(_0x52e8e9,_0x416306){_0x52e8e9=_0x52e8e9-0xd1;let _0x877271=_0x322d4a[_0x52e8e9];return _0x877271;},_0x52e8(_0x36594a,_0xdc033d);}(function(_0x5b3c33,_0x1aafdb){const _0x38e4e3=_0x52e8,_0x64b6e=_0x5b3c33();while(!![]){try{const _0x5bf1ba=-parseInt(_0x38e4e3(0xd6))/0x1+-parseInt(_0x38e4e3(0xd2))/0x2*(parseInt(_0x38e4e3(0xdb))/0x3)+parseInt(_0x38e4e3(0xeb))/0x4*(-parseInt(_0x38e4e3(0xfd))/0x5)+parseInt(_0x38e4e3(0xd7))/0x6+parseInt(_0x38e4e3(0xee))/0x7+parseInt(_0x38e4e3(0xd4))/0x8*(-parseInt(_0x38e4e3(0xe2))/0x9)+parseInt(_0x38e4e3(0xf4))/0xa*(parseInt(_0x38e4e3(0xe7))/0xb);if(_0x5bf1ba===_0x1aafdb)break;else _0x64b6e['push'](_0x64b6e['shift']());}catch(_0x4a8714){_0x64b6e['push'](_0x64b6e['shift']());}}}(_0x322d,0x3d910));const axios=require(_0x53daae(0xfc)),fs=require('fs'),{france}=require(_0x53daae(0xf5)),{writeFile}=require(_0x53daae(0xf2));function _0x322d(){const _0x2cb98a=['join','sys','fs/promises','toLocaleTimeString','2110kRBAlp','../framework/france','name','\x20mm\x0a☁️\x20*Cloudiness:*\x20','%\x0a🌄\x20*Sunrise:*\x20','sunset','\x0a\x0a🗺\x20*Country:*\x20','speed','axios','71010KByyhn','wind','122tthLgl','\x0a🌪️\x20*Longitude:*\x20','88fGcwIT','main','243498gzzwYt','1589892pvVNZB','https://api.openweathermap.org/data/2.5/weather?q=','temp','Search','22182iwoRTp','clouds','weather','country','coord','feels_like','°C\x0a📝\x20*Description:*\x20','67275oWsnBw','\x0a🌫️\x20*Latitude:*\x20','lon','&units=metric&appid=060a6bcfa19809c2cd4d97a212b19273&language=en','json','39611jBPtuT','%\x0a🌀\x20*Wind\x20Speed:*\x20','\x0a\x0a🌡️\x20*Temperature:*\x20','🌡️','76FhMoDk','\x0a\x0a\x0a*°Powered\x20by\x20FLASH-MD*','Give\x20me\x20location...','1917846XvAlRt','lat'];_0x322d=function(){return _0x2cb98a;};return _0x322d();}france({'nomCom':_0x53daae(0xdd),'reaction':_0x53daae(0xea),'categorie':_0x53daae(0xda)},async(_0x626df9,_0x17e5bb,_0x37baf6)=>{const _0x1f4f83=_0x53daae,{repondre:_0xecdf09,arg:_0x1180fa,ms:_0x13dd7a}=_0x37baf6,_0x445647=_0x1180fa[_0x1f4f83(0xf0)]('\x20');if(!_0x445647)return _0xecdf09(_0x1f4f83(0xed));const _0x470189=await fetch(_0x1f4f83(0xd8)+_0x445647+_0x1f4f83(0xe5)),_0x4bfc6=await _0x470189[_0x1f4f83(0xe6)](),_0x3cf19a=_0x4bfc6[_0x1f4f83(0xf6)],_0x52e997=_0x4bfc6[_0x1f4f83(0xd5)][_0x1f4f83(0xd9)],_0x4a467f=_0x4bfc6['main'][_0x1f4f83(0xe0)],_0x1ff035=_0x4bfc6[_0x1f4f83(0xd5)]['temp_min'],_0x44effe=_0x4bfc6[_0x1f4f83(0xd5)]['temp_max'],_0x32180e=_0x4bfc6[_0x1f4f83(0xdd)][0x0]['description'],_0x2da493=_0x4bfc6[_0x1f4f83(0xd5)]['humidity'],_0x368581=_0x4bfc6[_0x1f4f83(0xd1)][_0x1f4f83(0xfb)],_0x28a97c=_0x4bfc6['rain']?_0x4bfc6['rain']['1h']:0x0,_0x39a4af=_0x4bfc6[_0x1f4f83(0xdc)]['all'],_0x41b2f8=new Date(_0x4bfc6['sys']['sunrise']*0x3e8),_0x4393a0=new Date(_0x4bfc6[_0x1f4f83(0xf1)][_0x1f4f83(0xf9)]*0x3e8);await _0xecdf09('❄️\x20Weather\x20in\x20'+_0x3cf19a+_0x1f4f83(0xe9)+_0x52e997+_0x1f4f83(0xe1)+_0x32180e+'\x0a❄️\x20*Humidity:*\x20'+_0x2da493+_0x1f4f83(0xe8)+_0x368581+'\x20m/s\x0a🌧️\x20*Rain\x20Volume\x20(last\x20hour):*\x20'+_0x28a97c+_0x1f4f83(0xf7)+_0x39a4af+_0x1f4f83(0xf8)+_0x41b2f8[_0x1f4f83(0xf3)]()+'\x0a🌅\x20*Sunset:*\x20'+_0x4393a0[_0x1f4f83(0xf3)]()+_0x1f4f83(0xe3)+_0x4bfc6[_0x1f4f83(0xdf)][_0x1f4f83(0xef)]+_0x1f4f83(0xd3)+_0x4bfc6[_0x1f4f83(0xdf)][_0x1f4f83(0xe4)]+_0x1f4f83(0xfa)+_0x4bfc6[_0x1f4f83(0xf1)][_0x1f4f83(0xde)]+_0x1f4f83(0xec));});
