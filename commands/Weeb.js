7fb3360b37cf1281fe8e7a2c6e98c8db1b28415e
France King <151246926+franceking1@users.noreply.github.com>
2024-04-18 18:34:43 +0300

    🐐 🔵

---
+++ commands/Weeb.js
@@ -0,0 +1,162 @@
+
+/** 
+
+🇫‌🇱‌🇦‌🇸‌🇭‌-🇲‌🇩‌ 
+
+  𝗖𝗼𝗽𝘆𝗿𝗶𝗴𝗵𝘁 (𝗖) 2024.
+ 𝗟𝗶𝗰𝗲𝗻𝘀𝗲𝗱 𝘂𝗻𝗱𝗲𝗿 𝘁𝗵𝗲  𝗠𝗜𝗧 𝗟𝗶𝗰𝗲𝗻𝘀𝗲;
+ 𝗬𝗼𝘂 𝗺𝗮𝘆 𝗻𝗼𝘁 𝘂𝘀𝗲 𝘁𝗵𝗶𝘀 𝗳𝗶𝗹𝗲 𝗲𝘅𝗰𝗲𝗽𝘁 𝗶𝗻 𝗰𝗼𝗺𝗽𝗹𝗶𝗮𝗻𝗰𝗲 𝘄𝗶𝘁𝗵 𝘁𝗵𝗲 𝗟𝗶𝗰𝗲𝗻𝘀𝗲.
+ 𝗜𝘁 𝗶𝘀 𝘀𝘂𝗽𝗽𝗹𝗶𝗲𝗱 𝗶𝗻 𝘁𝗵𝗲 𝗵𝗼𝗽𝗲 𝘁𝗵𝗮𝘁 𝗶𝘁 𝗺𝗮𝘆 𝗯𝗲 𝘂𝘀𝗲𝗳𝘂𝗹.
+ * @𝗽𝗿𝗼𝗷𝗲𝗰𝘁_𝗻𝗮𝗺𝗲 : 𝗙𝗹𝗮𝘀𝗵 𝗠𝗗, 𝗮 𝘀𝗶𝗺𝗽𝗹𝗲 𝗮𝗻𝗱 𝗲𝗮𝘀𝘆 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 𝘂𝘀𝗲𝗿 𝗯𝗼𝘁 
+ * @𝗼𝘄𝗻𝗲𝗿: 𝗙𝗿𝗮𝗻𝗰𝗲 𝗞𝗶𝗻𝗴 
+ 
+ **/
+
+
+
+
+
+
+const axios = require('axios');
+const fs = require('fs');
+const { france } = require("../framework/france");
+const { writeFile } = require('fs/promises')
+
+// Commande waifu
+france({
+  nomCom: "waifu",
+  categorie: "Weeb",
+  reaction: "😏"
+},
+async (origineMessage, zk, commandeOptions) => {
+  const { repondre, ms } = commandeOptions;
+
+  const url = 'https://api.waifu.pics/sfw/waifu'; // Remplacez avec le lien réel de l'API waifu.pics
+
+  try {
+    
+    for (let i = 0; i < 5; i++) {
+      const response = await axios.get(url);
+      const imageUrl = response.data.url;
+
+      zk.sendMessage(origineMessage, { image: { url: imageUrl } }, { quoted: ms });
+    }
+  } catch (error) {
+    repondre('Erreur lors de la récupération des données :', error);
+  }
+});
+
+// Commande neko
+france({
+  nomCom: "neko",
+  categorie: "Weeb",
+  reaction: "😺"
+},
+async (origineMessage, zk, commandeOptions) => {
+  const { repondre, ms } = commandeOptions;
+
+  const url = 'https://api.waifu.pics/sfw/neko'; // Remplacez avec le lien réel de l'API waifu.pics ou une autre API de nekos
+
+  try {
+    for (let i = 0; i < 5; i++) {
+      const response = await axios.get(url);
+      const imageUrl = response.data.url;
+
+      zk.sendMessage(origineMessage, { image: { url: imageUrl } }, { quoted: ms });
+    }
+  } catch (error) {
+    repondre('Erreur lors de la récupération des données :', error);
+  }
+});
+
+// Commande shinobu
+france({
+  nomCom: "shinobu",
+  categorie: "Weeb",
+  reaction: "🦋"
+},
+async (origineMessage, zk, commandeOptions) => {
+  const { repondre, ms } = commandeOptions;
+
+  const url = 'https://api.waifu.pics/sfw/shinobu'; // Remplacez avec le lien réel de l'API waifu.pics ou une autre API avec des images de Shinobu
+
+  try {
+    for (let i = 0; i < 5; i++) {
+      const response = await axios.get(url);
+      const imageUrl = response.data.url;
+
+      zk.sendMessage(origineMessage, { image: { url: imageUrl } }, { quoted: ms });
+    }
+  } catch (error) {
+    repondre('Erreur lors de la récupération des données :', error);
+  }
+});
+
+// Commande megumin
+france({
+  nomCom: "megumin",
+  categorie: "Weeb",
+  reaction: "💥"
+},
+async (origineMessage, zk, commandeOptions) => {
+  const { repondre, ms } = commandeOptions;
+
+  const url = 'https://api.waifu.pics/sfw/megumin'; // Remplacez avec le lien réel de l'API waifu.pics ou une autre API avec des images de Megumin
+
+  try {
+    for (let i = 0; i < 5; i++) {
+      const response = await axios.get(url);
+      const imageUrl = response.data.url;
+
+      zk.sendMessage(origineMessage,{ image: { url: imageUrl } }, { quoted: ms });
+    }
+  } catch (error) {
+    repondre('Erreur lors de la récupération des données :', error);
+  }
+});
+
+
+
+france({
+  nomCom: "cosplay",
+  categorie: "Weeb",
+  reaction: "😏"
+},
+async (origineMessage, zk, commandeOptions) => {
+  const { repondre, ms } = commandeOptions;
+
+
+
+  try {
+    for (let i = 0; i < 5; i++) {
+      let url = 'https://fantox-cosplay-api.onrender.com/'
+      
+   const response = await   axios.get(url, { responseType: 'arraybuffer' })
+
+  
+
+  const image = response.data;
+
+   await writeFile('./cosplay.jpg', image)
+      zk.sendMessage(origineMessage,{image : {url : `./cosplay.jpg`}},{quoted :ms}) }
+  
+  } catch (e) {
+    repondre("je reçois malheureusement une erreur : " + e);
+  }
+});
+
+
+france({nomCom:"couplepp",categorie: "Weeb",reaction : "💞"},async(dest,zk,commandeOptions)=>{ const {repondre , ms} = commandeOptions;
+    let api = 'https://smiling-hosiery-bear.cyclic.app/weeb/couplepp'
+  try {
+     repondre('she/he dont love you :)')
+ const result = await axios.get(api)
+  
+
+    zk.sendMessage(dest, { image: { url: result.data.male }, caption: `For Man` }, { quoted: ms })
+        zk.sendMessage(dest, { image: { url: result.data.female }, caption: `_For woman_` }, { quoted: ms })
+    
+  } catch (e) { repondre(e)}                                                                                        
+  
+}
+      )
