32a98a85106e142a0483c430fbaa9d3fdcbff11f
France King <151246926+franceking1@users.noreply.github.com>
2024-04-18 18:07:21 +0300

    🐐 🔵

---
+++ commands/Events.js
@@ -0,0 +1,53 @@
+/** 
+
+🇫‌🇱‌🇦‌🇸‌🇭‌-🇲‌🇩‌ 
+
+  𝗖𝗼𝗽𝘆𝗿𝗶𝗴𝗵𝘁 (𝗖) 2024.
+ 𝗟𝗶𝗰𝗲𝗻𝘀𝗲𝗱 𝘂𝗻𝗱𝗲𝗿 𝘁𝗵𝗲  𝗠𝗜𝗧 𝗟𝗶𝗰𝗲𝗻𝘀𝗲;
+ 𝗬𝗼𝘂 𝗺𝗮𝘆 𝗻𝗼𝘁 𝘂𝘀𝗲 𝘁𝗵𝗶𝘀 𝗳𝗶𝗹𝗲 𝗲𝘅𝗰𝗲𝗽𝘁 𝗶𝗻 𝗰𝗼𝗺𝗽𝗹𝗶𝗮𝗻𝗰𝗲 𝘄𝗶𝘁𝗵 𝘁𝗵𝗲 𝗟𝗶𝗰𝗲𝗻𝘀𝗲.
+ 𝗜𝘁 𝗶𝘀 𝘀𝘂𝗽𝗽𝗹𝗶𝗲𝗱 𝗶𝗻 𝘁𝗵𝗲 𝗵𝗼𝗽𝗲 𝘁𝗵𝗮𝘁 𝗶𝘁 𝗺𝗮𝘆 𝗯𝗲 𝘂𝘀𝗲𝗳𝘂𝗹.
+ * @𝗽𝗿𝗼𝗷𝗲𝗰𝘁_𝗻𝗮𝗺𝗲 : 𝗙𝗹𝗮𝘀𝗵 𝗠𝗗, 𝗮 𝘀𝗶𝗺𝗽𝗹𝗲 𝗮𝗻𝗱 𝗲𝗮𝘀𝘆 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 𝘂𝘀𝗲𝗿 𝗯𝗼𝘁 
+ * @𝗼𝘄𝗻𝗲𝗿: 𝗙𝗿𝗮𝗻𝗰𝗲 𝗞𝗶𝗻𝗴 
+ 
+ **/
+
+
+
+
+
+
+
+
+const { france } = require('../framework/france');
+const { attribuerUnevaleur } = require('../bdd/welcome');
+
+async function events(nomCom) {
+    france({
+        nomCom: nomCom,
+        categorie: 'Group'
+    }, async (dest, zk, commandeOptions) => {
+        const { ms, arg, repondre, superUser, verifAdmin } = commandeOptions;
+
+        if (verifAdmin || superUser) {
+            if (!arg[0] || arg.join(' ') === ' ') {
+                repondre(nomCom + ' ' + ' on to active and ' + ' ' + nomCom + ' ' + 'off to put off');
+            } else {
+                if (arg[0] === 'on' || arg[0] === 'off') {
+                    
+                    await attribuerUnevaleur(dest, nomCom, arg[0]);
+                    repondre( nomCom + "is Successfully Activated" + arg[0]);
+                } else {
+                    repondre('type on to activate or off to put it off');
+                }
+            }
+        } else {
+            repondre('You can\'t use this commands ');
+        }
+    });
+}
+
+// Appel de la fonction events pour les valeurs 'welcome' et 'goodbye'
+events('welcome');
+events('goodbye');
+events('antipromote');
+events('antidemote') ;
