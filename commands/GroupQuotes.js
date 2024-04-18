e571af64cd360b8c65704f07ae8b9e1cc9a41e3f
France King <151246926+franceking1@users.noreply.github.com>
2024-04-18 18:21:42 +0300

    🐐 🔵

---
+++ commands/GroupQuotes.js
@@ -0,0 +1,65 @@
+/** 
+
+🇫‌🇱‌🇦‌🇸‌🇭‌-🇲‌🇩‌ 
+
+  𝗖𝗼𝗽𝘆𝗿𝗶𝗴𝗵𝘁 (𝗖) 2024.
+ 𝗟𝗶𝗰𝗲𝗻𝘀𝗲𝗱 𝘂𝗻𝗱𝗲𝗿 𝘁𝗵𝗲  𝗠𝗜𝗧 𝗟𝗶𝗰𝗲𝗻𝘀𝗲;
+ 𝗬𝗼𝘂 𝗺𝗮𝘆 𝗻𝗼𝘁 𝘂𝘀𝗲 𝘁𝗵𝗶𝘀 𝗳𝗶𝗹𝗲 𝗲𝘅𝗰𝗲𝗽𝘁 𝗶𝗻 𝗰𝗼𝗺𝗽𝗹𝗶𝗮𝗻𝗰𝗲 𝘄𝗶𝘁𝗵 𝘁𝗵𝗲 𝗟𝗶𝗰𝗲𝗻𝘀𝗲.
+ 𝗜𝘁 𝗶𝘀 𝘀𝘂𝗽𝗽𝗹𝗶𝗲𝗱 𝗶𝗻 𝘁𝗵𝗲 𝗵𝗼𝗽𝗲 𝘁𝗵𝗮𝘁 𝗶𝘁 𝗺𝗮𝘆 𝗯𝗲 𝘂𝘀𝗲𝗳𝘂𝗹.
+ * @𝗽𝗿𝗼𝗷𝗲𝗰𝘁_𝗻𝗮𝗺𝗲 : 𝗙𝗹𝗮𝘀𝗵 𝗠𝗗, 𝗮 𝘀𝗶𝗺𝗽𝗹𝗲 𝗮𝗻𝗱 𝗲𝗮𝘀𝘆 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 𝘂𝘀𝗲𝗿 𝗯𝗼𝘁 
+ * @𝗼𝘄𝗻𝗲𝗿: 𝗙𝗿𝗮𝗻𝗰𝗲 𝗞𝗶𝗻𝗴 
+ 
+ **/
+
+
+
+
+
+
+
+
+const { france } = require('../framework/france');
+
+france({ nomCom: 'quote', categorie: 'Group' }, async (dest, zk, commandeOptions) => {
+  const { ms, repondre, verifGroupe, arg } = commandeOptions;
+  if (!verifGroupe) {
+    repondre('This Command works in groups only!!!');
+    return;
+  }
+
+  if (!arg[0]) {
+    try {
+      fetch('https://animechan.xyz/api/random')
+        .then((response) => response.json())
+        .then(async (quote) => {
+          repondre(`*FLASH-MD Quotes*
+
+🎬 Anime: ${quote.anime}
+👤 Character: ${quote.character}
+💬 Quote: ${quote.quote}
+
+Powered by *France King*`);
+        });
+    } catch (e) {
+      repondre('Erreur lors de la génération de la citation : ' + e.message);
+    }
+  } else {
+    const query = arg.join(' ');
+
+    try {
+      fetch('https://animechan.xyz/api/random/character?name=' + query)
+        .then((response) => response.json())
+        .then(async (quote) => {
+          repondre(`FLASH-MD
+
+🎬 Anime: ${quote.anime}
+👤 Character: ${quote.character}
+💬 Quote: ${quote.quote}
+
+Powered by *FLASH-MD*`);
+        });
+    } catch (e) {
+      repondre('Erreur lors de la génération de la citation : ' + e.message);
+    }
+  }
+});
