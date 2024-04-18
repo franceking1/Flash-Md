ea9cf03d5293dcc91f45b36b9c8b9c3f5a5c40a9
France King <151246926+franceking1@users.noreply.github.com>
2024-04-18 18:12:37 +0300

    🐐 🔵

---
+++ commands/Heroku.js
@@ -0,0 +1,113 @@
+
+/** 
+
+🇫‌🇱‌🇦‌🇸‌🇭‌-🇲‌🇩‌ 
+
+  𝗖𝗼𝗽𝘆𝗿𝗶𝗴𝗵𝘁 (𝗖) 2024.
+ 𝗟𝗶𝗰𝗲𝗻𝘀𝗲𝗱 𝘂𝗻𝗱𝗲𝗿 𝘁𝗵𝗲  𝗠𝗜𝗧 𝗟𝗶𝗰𝗲𝗻𝘀𝗲;
+ 𝗬𝗼𝘂 𝗺𝗮𝘆 𝗻𝗼𝘁 𝘂𝘀𝗲 𝘁𝗵𝗶𝘀 𝗳𝗶𝗹𝗲 𝗲𝘅𝗰𝗲𝗽𝘁 𝗶𝗻 𝗰𝗼𝗺𝗽𝗹𝗶𝗮𝗻𝗰𝗲 𝘄𝗶𝘁𝗵 𝘁𝗵𝗲 𝗟𝗶𝗰𝗲𝗻𝘀𝗲.
+ 𝗜𝘁 𝗶𝘀 𝘀𝘂𝗽𝗽𝗹𝗶𝗲𝗱 𝗶𝗻 𝘁𝗵𝗲 𝗵𝗼𝗽𝗲 𝘁𝗵𝗮𝘁 𝗶𝘁 𝗺𝗮𝘆 𝗯𝗲 𝘂𝘀𝗲𝗳𝘂𝗹.
+ * @𝗽𝗿𝗼𝗷𝗲𝗰𝘁_𝗻𝗮𝗺𝗲 : 𝗙𝗹𝗮𝘀𝗵 𝗠𝗗, 𝗮 𝘀𝗶𝗺𝗽𝗹𝗲 𝗮𝗻𝗱 𝗲𝗮𝘀𝘆 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 𝘂𝘀𝗲𝗿 𝗯𝗼𝘁 
+ * @𝗼𝘄𝗻𝗲𝗿: 𝗙𝗿𝗮𝗻𝗰𝗲 𝗞𝗶𝗻𝗴 
+ 
+ **/
+
+
+
+
+
+
+
+
+
+
+
+
+const { france } = require('../framework/france');
+const s = require('../set')
+
+
+france(
+    {
+        nomCom : "setvar",
+        categorie : "heroku"
+    }, async (dest , zk , commandeOptions) =>{
+
+       const {ms,repondre,superUser , arg} = commandeOptions ;
+       
+       if(!superUser){repondre('only Mods can use this commande');return};
+       if(!arg[0] || !(arg.join('').split('='))) {repondre('Bad format ; Exemple of using :\nSetvar OWNER_NAME=France King');return};
+     
+    const text = arg.join(" ")
+     const Heroku = require("heroku-client");
+   
+     const heroku = new Heroku({
+        token: s.HEROKU_APY_KEY,
+      });
+
+     let baseURI = "/apps/" + s.HEROKU_APP_NAME;
+        await heroku.patch(baseURI + "/config-vars", {
+          body: {
+                  [text.split('=')[0]]: text.split('=')[1],
+          },
+        });
+        await repondre('That Heroku var is changing,The bot is rebooting....')
+    }
+);
+
+france(
+    {
+        nomCom : "allvar",
+        categorie : "heroku"
+    }, async (dest , zk , commandeOptions) =>{
+
+       const {ms,repondre,superUser , arg} = commandeOptions ;
+       
+       if(!superUser){repondre('only mods can use this commande');return}; 
+      
+            const Heroku = require("heroku-client");
+
+			const heroku = new Heroku({
+				token: s.HEROKU_APY_KEY,
+			});
+			let baseURI = "/apps/" + s.HEROKU_APP_NAME;
+
+            let h = await heroku.get(baseURI+'/config-vars')
+let str = '*All my HEROKU Vars*\n\n'
+for (vr in h) {
+str+= '⚡ *'+vr+'* '+'= '+h[vr]+'\n'
+}
+ repondre(str)
+
+
+}
+
+);       
+
+
+    france(
+        {
+            nomCom : "getvar",
+            categorie : "heroku"
+        }, async (dest , zk , commandeOptions) =>{
+    
+           const {ms,repondre,superUser , arg} = commandeOptions ;
+           
+           if(!superUser){repondre('Only Mods can use this command');return}; 
+           if(!arg[0]) {repondre('insert the variable name in capital letter'); return} ;
+      
+           try {
+            const Heroku = require("heroku-client");
+               
+            const heroku = new Heroku({
+              token: s.HEROKU_API_KEY,
+            });
+            let baseURI = "/apps/" + s.HEROKU_APP_NAME;
+        let h = await heroku.get(baseURI+'/config-vars')
+        for (vr in h) {
+        if( arg.join(' ') ===vr ) return  repondre( vr+'= '+h[vr]) 	;
+        } 
+        
+        } catch(e) {repondre('Error' + e)}
+   
+        });
