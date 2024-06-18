3ab6edbe035c1e6b219f2902eb72d4931f7c5889
France King <151246926+franceking1@users.noreply.github.com>
2024-06-18 08:50:04 +0300

    🐐 🔵

--- set.js
+++ set.js
@@ -8,7 +8,7 @@ const DATABASE_URL = process.env.DATABASE_URL === undefined
     ? databasePath
     : process.env.DATABASE_URL;
 module.exports = { session: process.env.SESSION_ID || 'FLASH-MD-WA-BOT;;;=>',
-    PREFIXE: process.env.PREFIX || "+",
+    PREFIXE: process.env.PREFIX || ".",
     OWNER_NAME: process.env.OWNER_NAME || "France King",
     OWNER_NUMBER: process.env.OWNER_NUMBER || "254742063632", 
              
@@ -20,14 +20,14 @@ CHATBOT: process.env.CHAT_BOT || "off",
     OPENAI_API_KEY : process.env.OPENAI_API_KEY || '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',
     URL : process.env.BOT_MENU_LINKS || 'https://static.animecorner.me/2023/08/op2.jpg',
     MODE: process.env.MODE || "private",
-    PM_PERMIT: process.env.PM_PERMIT || 'no',
+    PM_PERMIT: process.env.PM_PERMIT || 'on',
     HEROKU_APP_NAME : process.env.HEROKU_APP_NAME,
     HEROKU_APY_KEY : process.env.HEROKU_API_KEY ,
     WARN_COUNT : process.env.WARN_COUNT || '3' ,
     PRESENCE : process.env.PRESENCE || '',
     //GPT : process.env.OPENAI_API_KEY || '',
-    DP : process.env.STARTING_BOT_MESSAGE || "yes",
-    ADM : process.env.ANTI_DELETE_MESSAGE || 'yes',
+    DP : process.env.STARTING_BOT_MESSAGE || "on",
+    ADM : process.env.ANTI_DELETE_MESSAGE || 'off',
     DATABASE_URL,
     DATABASE: DATABASE_URL === databasePath
         ? "postgres://db_7xp9_user:6hwmTN7rGPNsjlBEHyX49CXwrG7cDeYi@dpg-cj7ldu5jeehc73b2p7g0-a.oregon-postgres.render.com/db_7xp9" : "postgres://db_7xp9_user:6hwmTN7rGPNsjlBEHyX49CXwrG7cDeYi@dpg-cj7ldu5jeehc73b2p7g0-a.oregon-postgres.render.com/db_7xp9",
