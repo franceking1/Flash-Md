9e00c8d7e6b8150c29e5ef5a5a08786ebfe571bd
France King <151246926+franceking1@users.noreply.github.com>
2024-09-16 11:49:48 +0300

    Delete france directory

--- france/app.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.reagir = void 0;
-async function reagir(dest, zok, msg, emoji) {
-    await zok.sendMessage(dest, { react: { text: emoji, key: msg.key } });
-}
-exports.reagir = reagir;
