dbcd973435ead07d8bbc067c324a064f4958b62f
France King <151246926+franceking1@users.noreply.github.com>
2024-11-15 09:57:45 +0300

    Update set.js

--- set.js
+++ set.js
@@ -9,8 +9,9 @@ if (fs.existsSync('set.env')) {
 
 const databasePath = path.join(__dirname, './database.db');
 const DATABASE_URL = process.env.DATABASE_URL === undefined ? databasePath : process.env.DATABASE_URL;
+
 module.exports = {
-    session: process.env.SESSION_ID || '',
+    session: process.env.SESSION_ID || 'FLASH-MD-WA-BOT;;;=>',
     PREFIXES: (process.env.PREFIX || '').split(',').map(prefix => prefix.trim()).filter(Boolean),
     OWNER_NAME: process.env.OWNER_NAME || "France King",
     OWNER_NUMBER: process.env.OWNER_NUMBER || "254105915061",
@@ -19,37 +20,24 @@ module.exports = {
     CHATBOT: process.env.CHAT_BOT || "off",
     AUTO_DOWNLOAD_STATUS: process.env.AUTO_SAVE_STATUS || 'off',
     A_REACT: process.env.AUTO_REACTION || 'on',
+    L_S: process.env.STATUS_LIKE || 'on',
     AUTO_BLOCK: process.env.BLOCK_ALL || 'off',
-    URL: process.env.BOT_MENU_LINKS || 'https://static.animecorner.me/2023/08/op2.jpg',
+    URL: process.env.MENU_LINKS || 'https://files.catbox.moe/c2jdkw.jpg',
     MODE: process.env.BOT_MODE || "private",
     PM_PERMIT: process.env.PM_PERMIT || 'on',
     HEROKU_APP_NAME: process.env.HEROKU_APP_NAME,
     HEROKU_API_KEY: process.env.HEROKU_API_KEY,
     WARN_COUNT: process.env.WARN_COUNT || '3',
-    PRESENCE: process.env.PRESENCE || 'online',
+    PRESENCE: process.env.PRESENCE || '',
     ADM: process.env.ANTI_DELETE || 'on',
     TZ: process.env.TIME_ZONE || 'Africa/Nairobi',
     DP: process.env.STARTING_MESSAGE || "on",
     ANTICALL: process.env.ANTICALL || 'on',
     DATABASE_URL,
     DATABASE: DATABASE_URL === databasePath
-        ? "postgresql://giftedtech_ke:9BzoUeUQO2owLEsMjz5Vhshva91bxF2X@dpg-crice468ii6s73f1nkt0-a.oregon-postgres.render.com/api_gifted_tech"
-        : "postgresql://giftedtech_ke:9BzoUeUQO2owLEsMjz5Vhshva91bxF2X@dpg-crice468ii6s73f1nkt0-a.oregon-postgres.render.com/api_gifted_tech",
-    /* new Sequelize({
-        dialect: 'sqlite',
-        storage: DATABASE_URL,
-        logging: false,
-    })
-    : new Sequelize(DATABASE_URL, {
-        dialect: 'postgres',
-        ssl: true,
-        protocol: 'postgres',
-        dialectOptions: {
-            native: true,
-            ssl: { require: true, rejectUnauthorized: false },
-        },
-        logging: false,
-    }), */
+        ? "postgresql://flashmd_user:JlUe2Vs0UuBGh0sXz7rxONTeXSOra9XP@dpg-cqbd04tumphs73d2706g-a/flashmd"
+        : "postgresql://flashmd_user:JlUe2Vs0UuBGh0sXz7rxONTeXSOra9XP@dpg-cqbd04tumphs73d2706g-a/flashmd",
+    W_M: null, // Add this line
 };
 
 // Watch for changes in this file and reload it automatically
